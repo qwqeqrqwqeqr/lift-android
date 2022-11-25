d4d156d76ef747263a15c35d43479507a9fa2ced
qwqeqrqwqeqr <qwqeqrqwqeqr@github.com>
2022-11-25 21:34:55 +0900

    [Test] : add insert testInsertRoutineSet, workCategory dao test module routineset dao의 insert 기능을 테스트하는 코드를 완성합니다 workcategory dao의 테스트 기반 모듈을 설정해놓습니다.

--- core/database/src/androidTest/java/com/gradation/lift/database/data/RoutineSetDaoTest.kt
+++ core/database/src/androidTest/java/com/gradation/lift/database/data/RoutineSetDaoTest.kt
@@ -1,19 +1,15 @@
 package com.gradation.lift.database.data
 
-import android.content.Context
 import androidx.test.filters.SmallTest
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.test.core.app.ApplicationProvider
 import com.gradation.lift.database.dao.RoutineSetDao
 import com.gradation.lift.database.util.TestDataGenerator
 import com.gradation.lift.database.di.LiftDatabase
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import com.google.common.truth.Truth.assertThat
-import com.gradation.lift.database.di.TestDatabaseModule.provideInMemoryDatabase
-import kotlinx.coroutines.flow.toList
-import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.*
 import org.junit.*
 import javax.inject.Inject
 import javax.inject.Named
@@ -44,13 +40,27 @@ class RoutineSetDaoTest  {
 
 
 
+    @After
+    fun tearDown() {
+        database.close()
+    }
 
     @Test
-    fun testInsertRoutineSet() = runTest {
+    fun testInsertRoutineSet() = runTest{
         val routineSetEntity = TestDataGenerator.testRoutineSetOne
         routineSetDao.insertRoutineSet(routineSetEntity)
-        val allEntries = routineSetDao.getAllRoutineSetEntries().toList()
-        assertThat(allEntries.size).isEqualTo(1)
+        val result =  routineSetDao.getAllRoutineSetEntries().first()
+
+        Assert.assertEquals(
+            listOf(routineSetEntity.name),
+            result.map { it.name }
+        )
+        Assert.assertEquals(
+            listOf(routineSetEntity.routineList),
+            result.map { it.routineList }
+        )
     }
+
+
 
 }
--- core/database/src/androidTest/java/com/gradation/lift/database/data/WorkCategoryDaoTest.kt
+++ core/database/src/androidTest/java/com/gradation/lift/database/data/WorkCategoryDaoTest.kt
@@ -1,4 +1,42 @@
 package com.gradation.lift.database.data
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.gradation.lift.database.dao.RoutineSetDao
+import com.gradation.lift.database.dao.WorkCategoryDao
+import com.gradation.lift.database.di.LiftDatabase
+import com.gradation.lift.database.util.TestDataGenerator
+import dagger.hilt.android.testing.HiltAndroidRule
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.*
+import javax.inject.Inject
+import javax.inject.Named
+
 class WorkCategoryDaoTest {
+
+    @Inject
+    @Named("test_database")
+    lateinit var database: LiftDatabase
+    private lateinit var workCategoryDao: WorkCategoryDao
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+        workCategoryDao = database.workCategoryDao()
+    }
+
+    @After
+    fun tearDown() {
+        database.close()
+    }
+
+
+  
 }
