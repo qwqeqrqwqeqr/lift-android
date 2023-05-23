564bf0a8dbf77add24e406c9593a30a372fe4558
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-05-23 17:15:59 +0900

    [Add] : add design system component

--- core/designsystem/src/main/java/com/gradation/lift/designsystem/component/Button.kt
+++ core/designsystem/src/main/java/com/gradation/lift/designsystem/component/Button.kt
@@ -1,12 +1,13 @@
 package com.gradation.lift.designsystem.component
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
 @Composable
@@ -25,6 +26,54 @@ fun LiftButton(
             containerColor = MaterialTheme.colorScheme.primary,
         ),
         contentPadding = contentPadding,
+        content = content,
+    )
+}
+
+@Composable
+fun LiftOutlinedButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+    content: @Composable RowScope.() -> Unit,
+) {
+    OutlinedButton(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        colors = ButtonDefaults.outlinedButtonColors(
+            contentColor = MaterialTheme.colorScheme.onBackground,
+        ),
+        border = BorderStroke(
+            width = 1.dp,
+            color = if (enabled) {
+                MaterialTheme.colorScheme.outline
+            } else {
+                MaterialTheme.colorScheme.onSurface.copy(
+                    alpha = 0.1f,
+                )
+            },
+        ),
+        contentPadding = contentPadding,
+        content = content,
+    )
+}
+
+@Composable
+fun LiftTextButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    content: @Composable RowScope.() -> Unit,
+) {
+    TextButton(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        colors = ButtonDefaults.textButtonColors(
+            contentColor = MaterialTheme.colorScheme.onBackground,
+        ),
         content = content,
     )
 }
