f20f2089ba7772f0dc46057a6cc07d6af716abe1
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-05-25 13:17:25 +0900

    [Update] : update routine service

--- core/network/src/main/java/com/gradation/lift/network/service/RoutineService.kt
+++ core/network/src/main/java/com/gradation/lift/network/service/RoutineService.kt
@@ -1,28 +1,30 @@
 package com.gradation.lift.network.service
 
 import com.gradation.lift.network.common.APIResultWrapper
-import com.gradation.lift.network.dto.routine.CreateRoutineSetRequestDto
-import com.gradation.lift.network.dto.routine.GetRoutineSetResponseDto
+import com.gradation.lift.network.dto.routine.*
 import com.gradation.lift.network.dto.work.GetWorkPartResponseDto
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface RoutineService {
 
-
     @GET("routine/routine-set/")
-    suspend fun getRoutineSet(): APIResultWrapper<List<GetRoutineSetResponseDto>>
+    suspend fun getRoutineSet(@Query("user_id") userID: String): APIResultWrapper<List<GetRoutineSetResponseDto>>
 
     @POST("routine/routine-set/")
     suspend fun createRoutineSet(createRoutineSetRequestDto: CreateRoutineSetRequestDto): APIResultWrapper<Boolean>
 
-
     @GET("routine/routine-set-by-date/")
-    suspend fun getRoutineSetByDate(): APIResultWrapper<List<GetWorkPartResponseDto>>
+    suspend fun getRoutineSetByDate(@Query("user_id") userID: String): APIResultWrapper<List<GetRoutineSetByDateResponseDto>>
 
     @GET("routine/routine")
-    suspend fun getRoutine(): APIResultWrapper<List<GetWorkPartResponseDto>>
+    suspend fun getRoutine(@Query("user_id") userID: String): APIResultWrapper<List<GetRoutineResponseDto>>
 
     @GET("routine/routine-by-date/")
-    suspend fun getRoutineByDate(): APIResultWrapper<List<GetWorkPartResponseDto>>
+    suspend fun getRoutineByDate(@Query("user_id") userID: String): APIResultWrapper<List<GetRoutineByDateResponseDto>>
+
+
+    //TODO  getRoutineByRoutineSet 추가할 것
+    //TODO  getRoutineSet 을 진행할 시, 동반되는 routine도 같이 부를것
 }
