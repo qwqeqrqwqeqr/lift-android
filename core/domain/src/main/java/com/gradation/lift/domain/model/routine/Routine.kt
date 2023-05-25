34a51d16adf624cfe6cdbf8426a7e480686b8725
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-05-25 15:37:30 +0900

    [Update] : update routine model

--- core/domain/src/main/java/com/gradation/lift/domain/model/routine/Routine.kt
+++ core/domain/src/main/java/com/gradation/lift/domain/model/routine/Routine.kt
@@ -1,7 +1,17 @@
 package com.gradation.lift.domain.model.routine
 
+import com.gradation.lift.domain.model.work.WorkCategory
+
 data class Routine(
+    val id: Int,
+    val routine_set_id: Int,
     val workCategory : Int,
     val workWeightList: List<Float>,
     val workRepetitionList: List<Int>,
+    val max_weight: Int,
+    val min_weight: Int,
+    val total_weight: Int,
+    val max_repetition: Int,
+    val min_repetition: Int,
+    val total_repetition: Int,
 )
--- core/network/src/main/java/com/gradation/lift/network/dto/routine/GetRoutineResponseDto.kt
+++ core/network/src/main/java/com/gradation/lift/network/dto/routine/GetRoutineResponseDto.kt
@@ -1,5 +1,6 @@
 package com.gradation.lift.network.dto.routine
 
+import com.gradation.lift.domain.model.routine.Routine
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -13,4 +14,22 @@ data class GetRoutineResponseDto (
     val max_repetition: Int,
     val min_repetition: Int,
     val total_repetition: Int,
-)
+){
+    fun toRoutine(): Routine =
+        Routine(
+            id=id,
+            routine_set_id=routine_set_id,
+            workCategory = routine.work_category,
+            workWeightList = routine.work_weight_list,
+            workRepetitionList = routine.work_repetition_list,
+            max_weight=max_weight,
+            min_weight=min_weight,
+            total_weight=total_weight,
+            max_repetition=max_repetition,
+            min_repetition=min_repetition,
+            total_repetition=total_repetition
+        )
+
+}
+
+//TODO Workcategory 다른형태로 받도록 수정할 것
