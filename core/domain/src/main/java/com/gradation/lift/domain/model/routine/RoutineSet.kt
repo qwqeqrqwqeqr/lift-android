3506723f318434985e9181c37e2ed42da8cbc9e8
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-05-25 13:55:02 +0900

    [Update] : update RoutineSet model

--- core/domain/src/main/java/com/gradation/lift/domain/model/routine/RoutineSet.kt
+++ core/domain/src/main/java/com/gradation/lift/domain/model/routine/RoutineSet.kt
@@ -1,9 +1,10 @@
 package com.gradation.lift.domain.model.routine
 
+import com.gradation.lift.domain.model.common.RepeatIntervalType
+
 data class RoutineSet(
     val id: Int,
     val short_description: String,
     val long_description: String,
-    val repeat_type: Int,
-    val repeat_interval: Int
+    val repeatIntervalType: RepeatIntervalType,
 )
