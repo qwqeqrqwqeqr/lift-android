5544de26a52b9661274234c34f2c2fca057331e5
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-06-14 15:46:19 +0900

    [Add] : add top bar icon

---
+++ core/designsystem/src/main/java/com/gradation/lift/designsystem/component/Topbar.kt
@@ -0,0 +1,31 @@
+package com.gradation.lift.designsystem.component
+
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.gradation.lift.designsystem.resource.Icon
+import com.gradation.lift.designsystem.resource.LiftIcon
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun LiftTopBar(
+    title : String,
+    onUpClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    TopAppBar(
+        title = {
+            Text(title)
+        },
+        modifier = modifier.statusBarsPadding(),
+        navigationIcon = {
+            IconButton(onClick = onUpClick) {
+                Icon.ImageVectorIcon(LiftIcon.ArrowBack)
+            }
+        },
+    )
+}
+
+//TODO top app bar 전용 color 추가
