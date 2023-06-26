95b5526ae45ca41a831646697affdfcca1301cda
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-06-26 17:27:59 +0900

    [Update] : update auth repository

--- core/data/src/main/java/com/gradation/lift/data/repository/DefaultAuthRepository.kt
+++ core/data/src/main/java/com/gradation/lift/data/repository/DefaultAuthRepository.kt
@@ -34,24 +34,24 @@ class DefaultAuthRepository @Inject constructor(
         }
     }
 
-//    override fun isSignedIn(): Flow<DataState<Boolean>> = flow {
-//
-//        if(userDataStoreDataSource.accessToken.first() == EMPTY_VALUE ||
-//            userDataStoreDataSource.refreshToken.first() == EMPTY_VALUE||
-//            userDataStoreDataSource.userId.first() == EMPTY_VALUE){
-////            emit(DataState.Fail(false))
-//        }
-//
-//
-//
-//        emit(DataState.Success(true))
-//
-//    }
+    override fun isSignedIn(): Flow<DataState<Boolean>> = flow {
+        if (userDataStoreDataSource.accessToken.first() == EMPTY_VALUE ||
+            userDataStoreDataSource.refreshToken.first() == EMPTY_VALUE ||
+            userDataStoreDataSource.userId.first() == EMPTY_VALUE
+        ) {
+            emit(DataState.Fail("로그인 되어있지 않습니다."))
+        }
 
-    override fun signOut(): Flow<DataState<Boolean>> = flow{
+        emit(DataState.Success(true))
+    }
 
-
-
-
+    override fun signOut(): Flow<DataState<Boolean>> = flow {
+        try {
+            userDataStoreDataSource.clearAll()
+            emit(DataState.Success(true))
+        }
+        catch(error: Exception){
+            emit(DataState.Error(error))
+        }
     }
 }
