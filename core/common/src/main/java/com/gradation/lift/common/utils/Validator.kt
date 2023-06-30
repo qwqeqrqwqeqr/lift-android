f0b6fe4c03a3fbb3ac96f4157ad0baf0fe8ffe03
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-06-30 17:23:58 +0900

    [Update] : update validator

--- core/common/src/main/java/com/gradation/lift/common/utils/Validator.kt
+++ core/common/src/main/java/com/gradation/lift/common/utils/Validator.kt
@@ -1,15 +1,18 @@
 package com.gradation.lift.common.utils
 
 import android.util.Patterns
+import java.util.regex.Pattern
 
+
+val passwordPattern: Pattern = Pattern.compile("""^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,16}$""")
+fun emailValidator(email: String): Boolean = Patterns.EMAIL_ADDRESS.matcher(email).matches()
+fun passwordValidator(password: String): Boolean = passwordPattern.matcher(password).matches()
+fun lengthValidator(string: String, lowLimit: Int, highLimit: Int): Boolean =
+    string.length in lowLimit..highLimit
 
 data class Validator(
     val status: Boolean = false,
     val message: String = "",
 )
-
-fun emailValidator(email: String): Boolean = Patterns.EMAIL_ADDRESS.matcher(email).matches()
-
-
 
 
