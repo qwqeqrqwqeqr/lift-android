90dd99481837ce2e7c24cdee1aa590bd9fb1baaf
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-07-22 22:04:07 +0900

    [Add] : add picture usecase

---
+++ core/domain/src/main/java/com/gradation/lift/domain/usecase/picture/GetRoutineSetPictureUseCase.kt
@@ -0,0 +1,16 @@
+package com.gradation.lift.domain.usecase.picture
+
+import com.gradation.lift.common.model.DataState
+import com.gradation.lift.domain.repository.HistoryRepository
+import com.gradation.lift.domain.repository.PictureRepository
+import com.gradation.lift.model.history.History
+import com.gradation.lift.model.picture.RoutineSetPicture
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetRoutineSetPictureUseCase @Inject constructor(
+    private val pictureRepository: PictureRepository
+){
+    operator fun invoke(): Flow<DataState<List<RoutineSetPicture>>> =
+        pictureRepository.getRoutineSetPicture()
+}
---
+++ core/domain/src/main/java/com/gradation/lift/domain/usecase/picture/GetUserProfilePictureUseCase.kt
@@ -0,0 +1,15 @@
+package com.gradation.lift.domain.usecase.picture
+
+import com.gradation.lift.common.model.DataState
+import com.gradation.lift.domain.repository.PictureRepository
+import com.gradation.lift.model.picture.RoutineSetPicture
+import com.gradation.lift.model.picture.UserProfilePicture
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetUserProfilePictureUseCase @Inject constructor(
+    private val pictureRepository: PictureRepository
+){
+    operator fun invoke(): Flow<DataState<List<UserProfilePicture>>> =
+        pictureRepository.getUserProfilePicture()
+}
