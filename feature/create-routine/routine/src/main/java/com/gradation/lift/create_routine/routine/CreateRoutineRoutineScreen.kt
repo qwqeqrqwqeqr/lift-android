0c18ad78e8d929c575a33c56b1f4dcca2dc7f488
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-08-03 00:21:11 +0900

    [Update] : update create-routine-routine

--- feature/create-routine/routine/src/main/java/com/gradation/lift/create_routine/routine/CreateRoutineRoutineScreen.kt
+++ feature/create-routine/routine/src/main/java/com/gradation/lift/create_routine/routine/CreateRoutineRoutineScreen.kt
@@ -2,15 +2,24 @@ package com.gradation.lift.create_routine.routine
 
 import android.annotation.SuppressLint
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
+import com.gradation.lift.designsystem.component.LiftBackTopBar
+import com.gradation.lift.designsystem.theme.LiftMaterialTheme
+import com.gradation.lift.designsystem.theme.LiftTheme
 import com.gradation.lift.feature.create_routine.data.CreateRoutineSharedViewModel
+import com.gradation.lift.model.routine.CreateRoutine
 import com.gradation.lift.navigation.Router
 
 @SuppressLint("UnrememberedGetBackStackEntry")
@@ -27,17 +36,56 @@ fun CreateRoutineRoutineRoute(
     val sharedViewModel: CreateRoutineSharedViewModel = hiltViewModel(crateRoutineBackStackEntry)
 
 
+    val tempCreateRoutine = sharedViewModel.tempRoutine.collectAsStateWithLifecycle()
+
 
     CreateRoutineRoutineScreen(
         modifier = modifier,
+        onBackClickTopBar = navigateCreateRoutineRoutineToFindWorkCategory,
+        tempCreateRoutine = tempCreateRoutine
     )
 
     BackHandler(onBack = navigateCreateRoutineRoutineToFindWorkCategory)
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CreateRoutineRoutineScreen(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    onBackClickTopBar: () -> Unit,
+    tempCreateRoutine: State<CreateRoutine>
 ) {
+    Scaffold(
+        topBar = {
+            LiftBackTopBar(
+                title = tempCreateRoutine.value.workCategory,
+                onBackClickTopBar = onBackClickTopBar,
+            )
+        }, modifier = modifier.fillMaxSize()
+    ) { PaddingValues ->
+        Surface(
+            color = LiftTheme.colorScheme.no5,
+            modifier = modifier.fillMaxSize()
+        ) {
+            Column(
+                modifier = modifier
+                    .padding(PaddingValues)
+                    .padding(16.dp)
+            ) {
+            }
+        }
+    }
+}
 
+@SuppressLint("UnrememberedMutableState")
+@Preview
+@Composable
+fun CreateRoutineRoutineScreenPreview() {
+    LiftMaterialTheme {
+        CreateRoutineRoutineScreen(
+            modifier = Modifier,
+            onBackClickTopBar = {},
+            tempCreateRoutine = mutableStateOf(CreateRoutine(workCategory = "턱걸이",workSetList = emptyList()))
+        )
+    }
 }
