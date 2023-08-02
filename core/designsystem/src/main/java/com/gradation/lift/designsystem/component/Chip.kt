f6f6d3c267e5a9aecee2a8b33d5c678854e78d4d
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-08-02 20:04:35 +0900

    [Add] : add custom chip component

---
+++ core/designsystem/src/main/java/com/gradation/lift/designsystem/component/Chip.kt
@@ -0,0 +1,74 @@
+package com.gradation.lift.designsystem.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.gradation.lift.designsystem.theme.LiftMaterialTheme
+import com.gradation.lift.designsystem.theme.LiftTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LiftFilterChip(
+    modifier: Modifier = Modifier,
+    text: String,
+    selected: Boolean,
+    onClick: () -> Unit
+) {
+    FilterChip(
+        modifier = modifier.padding(10.dp),
+        shape = RoundedCornerShape(6.dp),
+        border = FilterChipDefaults.filterChipBorder(
+            borderColor = Color.Transparent,
+            borderWidth = 0.dp
+        ),
+        colors = FilterChipDefaults.filterChipColors(
+            containerColor = LiftTheme.colorScheme.no1,
+            labelColor = LiftTheme.colorScheme.no9,
+            iconColor = LiftTheme.colorScheme.no9,
+
+            selectedContainerColor = LiftTheme.colorScheme.no4,
+            selectedLabelColor = LiftTheme.colorScheme.no5,
+            selectedLeadingIconColor = LiftTheme.colorScheme.no5,
+            selectedTrailingIconColor = LiftTheme.colorScheme.no5,
+        ),
+        selected = selected,
+        onClick = onClick,
+        label = {
+            Text(
+                text = text,
+                style = if (selected) LiftTheme.typography.no3 else LiftTheme.typography.no4
+            )
+        }
+    )
+}
+
+
+@Composable
+@Preview
+fun LiftFilterChipPreview() {
+
+    LiftMaterialTheme {
+        Column {
+            LiftFilterChip(
+                text = "리프트",
+                selected = true,
+                onClick = {}
+            )
+            LiftFilterChip(
+                text = "리프트",
+                selected = false,
+                onClick = {}
+            )
+        }
+
+    }
+}
