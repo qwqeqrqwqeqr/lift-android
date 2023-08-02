646e2c1239597be6bf431f0a81303f1abcf9f512
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-08-02 15:26:29 +0900

    [Update] : update gitaction

--- core/domain/src/test/java/com/gradation/lift/domain/DomainTestSuit.kt
+++ core/domain/src/test/java/com/gradation/lift/domain/DomainTestSuit.kt
@@ -1,15 +1,8 @@
 package com.gradation.lift.domain
 
-import com.gradation.lift.domain.usecase.DateUseCaseTest
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.junit.runner.RunWith
-import org.junit.runners.Suite
 
-@ExperimentalCoroutinesApi
-@RunWith(Suite::class)
-@Suite.SuiteClasses(
-    DateUseCaseTest::class
-)
+
+
 class DomainTestSuit {
 
 }
