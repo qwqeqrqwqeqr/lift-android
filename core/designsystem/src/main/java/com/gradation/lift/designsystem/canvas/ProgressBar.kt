c9f9a3e5ab337ddb74a2e981f8ad0f8940f105b0
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-08-05 15:13:54 +0900

    [Update] : update compose

--- core/designsystem/src/main/java/com/gradation/lift/designsystem/canvas/ProgressBar.kt
+++ core/designsystem/src/main/java/com/gradation/lift/designsystem/canvas/ProgressBar.kt
@@ -7,14 +7,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.gradation.lift.designsystem.brush.SkeletonBrush
 import com.gradation.lift.designsystem.theme.LiftMaterialTheme
 
 
@@ -33,14 +29,14 @@ fun LiftProgressBar(
                 color = Color(0xFFF2F2F5),
                 start = Offset(x = 0f, y = center.y),
                 end = Offset(x = size.width, y = center.y),
-                strokeWidth = 36f,
+                strokeWidth = 32f,
                 cap = StrokeCap.Round
             )
             drawLine(
                 color = Color(0xFF0080FF),
                 start = Offset(x = 0f, y = center.y),
                 end = Offset(x = progress /100 * size.width, y = center.y),
-                strokeWidth = 36f,
+                strokeWidth = 32f,
                 cap = StrokeCap.Round
             )
         }
