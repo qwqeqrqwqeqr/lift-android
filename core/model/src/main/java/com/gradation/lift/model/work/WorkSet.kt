7ec81728d8300c8abfb886bf62758edda93a8973
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-08-10 12:39:00 +0900

    docs(model): update WorkSet model docs
    - WorkSet Model 클래스에 대한 문서를 작성합니다.

--- core/model/src/main/java/com/gradation/lift/model/work/WorkSet.kt
+++ core/model/src/main/java/com/gradation/lift/model/work/WorkSet.kt
@@ -1,10 +1,10 @@
 package com.gradation.lift.model.work
 
 /**
- * @param weight
- * @param repetition
- *
- *
+ * [WorkSet]
+ * 운동의 무게와 횟수를 담아둔 모델
+ * @property weight  무게 (0.5 단위가 될 수 있기 때문에 float type 반영)
+ * @property repetition 횟수
  */
 data class WorkSet(
     val weight : Float,
