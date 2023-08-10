1466bd025d5be27ab9dca6c0f88d15518c9c8b93
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-08-10 17:11:25 +0900

    docs(model): update Routine model
    - picture 모델들에 대한 문서를 작성합니다.

--- core/model/src/main/java/com/gradation/lift/model/picture/UserProfilePicture.kt
+++ core/model/src/main/java/com/gradation/lift/model/picture/UserProfilePicture.kt
@@ -1,6 +1,11 @@
 package com.gradation.lift.model.picture
 
-
+/**
+ * [RoutineSetPicture]
+ * 루틴 세트 프로필에 등록되는 사진들의 정보를 저장하는 모델입니다.
+ * @property id  모델의 고유 아이디 번호
+ * @property url 해당 사진의 url 주소
+ **/
 data class UserProfilePicture(
     val id : Int,
     val url : String
