1cf13209b98c0b81a34d3367d308ee38e0866450
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-08-10 12:53:25 +0900

    docs(model): update User model docs
    - user 관련 모델들에 대한 문서를 작성합니다.
    - Gender 모델의 name 프로퍼티를 상수로 정의합니다.

--- core/model/src/main/java/com/gradation/lift/model/user/Gender.kt
+++ core/model/src/main/java/com/gradation/lift/model/user/Gender.kt
@@ -1,12 +1,21 @@
 package com.gradation.lift.model.user
 
+
+/**
+ * [Gender]
+ * 성별 정보를 담아둔 모델
+ * [String] 을 통해 [Gender] 접근
+ * [getGenderName] 또는 [getGenderValue]를 통해 [String]으로 변환
+ */
 sealed class Gender{
-    data class Male(val value: String = MALE_VALUE, val name: String = "남성") : Gender()
-    data class Female(val value: String = FEMALE_VALUE, val name: String = "여성") : Gender()
+    data class Male(val value: String = MALE_VALUE, val name: String = MALE_NAME_VALUE) : Gender()
+    data class Female(val value: String = FEMALE_VALUE, val name: String = FEMALE_NAME_VALUE) : Gender()
 
     companion object{
         const val MALE_VALUE = "male"
         const val FEMALE_VALUE = "female"
+        const val MALE_NAME_VALUE = "남성"
+        const val FEMALE_NAME_VALUE = "여성"
     }
 
     fun getGenderValue(): String = when(this){
--- core/model/src/main/java/com/gradation/lift/model/user/UserDetail.kt
+++ core/model/src/main/java/com/gradation/lift/model/user/UserDetail.kt
@@ -2,13 +2,24 @@ package com.gradation.lift.model.user
 
 import com.gradation.lift.model.common.UnitOfWeight
 
+
+/**
+ * [UserDetail]
+ * 사용자의 상세 정보를 담아둔 모델
+ * @property name  이름
+ * @property gender  성별
+ * @property height  키
+ * @property weight  몸무게
+ * @property profilePicture  프로필 사진
+ * @property unitOfWeight  무게 단위 (현재 미사용)
+ */
 data class UserDetail(
     val name: String,
     val gender: Gender,
     val height: Float,
     val weight: Float,
     val profilePicture: String,
-    val unitOfWeight: UnitOfWeight,
+    val unitOfWeight: UnitOfWeight = UnitOfWeight.Kg(),
 )
 
 
