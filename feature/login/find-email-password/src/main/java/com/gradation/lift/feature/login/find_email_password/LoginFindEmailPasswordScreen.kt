a895a1b0a11cd9553e622066c274aafb64ac2165
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-08-14 15:50:51 +0900

    feat: add find email password module - find email password module을 추가합니다.

---
+++ feature/login/find-email-password/src/main/java/com/gradation/lift/feature/login/find_email_password/LoginFindEmailPasswordScreen.kt
@@ -0,0 +1,15 @@
+package com.gradation.lift.feature.login.find_email_password
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+
+@Composable
+fun LoginFindEmailPasswordRoute(
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    viewModel: LoginFindEmailPasswordViewModel = hiltViewModel(),
+) {
+
+}
---
+++ feature/login/find-email-password/src/main/java/com/gradation/lift/feature/login/find_email_password/LoginFindEmailPasswordViewModel.kt
@@ -0,0 +1,11 @@
+package com.gradation.lift.feature.login.find_email_password
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginFindEmailPasswordViewModel  @Inject constructor(
+): ViewModel() {
+
+}
