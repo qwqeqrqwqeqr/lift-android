90a1e59eee708e145c4f2bf0c22a296d03dfb57e
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-08-14 15:50:11 +0900

    feat: add signin usecase - naver 로그인 usecase를 추가합니다 - kakao 로그인 usecase를 추가합니다

---
+++ core/domain/src/main/java/com/gradation/lift/domain/usecase/auth/SignInKakaoUseCase.kt
@@ -0,0 +1,14 @@
+package com.gradation.lift.domain.usecase.auth
+
+import com.gradation.lift.common.model.DataState
+import com.gradation.lift.domain.repository.AuthRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class SignInKakaoUseCase @Inject constructor(
+    private val authRepository: AuthRepository
+) {
+    operator fun invoke(): Flow<DataState<Boolean>> =
+        authRepository.signInKakao()
+}
+
