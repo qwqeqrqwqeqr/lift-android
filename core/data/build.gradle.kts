af819b7c7aa0972d7411688bd10dcc6a66ed9074
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-08-16 11:20:03 +0900

    build(data): update data gradle - oauth 모듈을 dependencies에 추가합니다.

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -29,4 +29,5 @@ dependencies{
     implementation(project(":core:datastore"))
     implementation(project(":core:database"))
     implementation(project(":core:model"))
+    implementation(project(":core:oauth"))
 }
