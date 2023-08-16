c5fcb10223eaf85faad207201325d179036af617
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-08-16 16:39:12 +0900

    feat(domain): add sign in usecase
    - sign in kakao usecase를 추가합니다.
    - sign in naver usecase를 추가합니다.
    - sign in default usecase의 반환형식을 수정합니다.

--- core/domain/src/main/java/com/gradation/lift/domain/usecase/auth/SignInKakaoUseCase.kt
+++ core/domain/src/main/java/com/gradation/lift/domain/usecase/auth/SignInKakaoUseCase.kt
@@ -2,14 +2,12 @@ package com.gradation.lift.domain.usecase.auth
 
 import com.gradation.lift.common.model.DataState
 import com.gradation.lift.domain.repository.AuthRepository
-import com.gradation.lift.model.model.auth.DefaultSignInInfo
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class SignInDefaultUseCase @Inject constructor(
+class SignInKakaoUseCase @Inject constructor(
     private val authRepository: AuthRepository
 ) {
-    operator fun invoke(signInInfo: DefaultSignInInfo): Flow<DataState<Unit>> =
-        authRepository.signInDefault(signInInfo =signInInfo)
+    operator fun invoke(): Flow<DataState<Unit>> =
+        authRepository.signInKakao()
 }
-
