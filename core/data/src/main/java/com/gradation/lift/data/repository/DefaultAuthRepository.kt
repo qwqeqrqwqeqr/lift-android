26d86c88f35697dac78e7f80fd0d1e95b91482d5
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-08-16 11:42:57 +0900

    feat(data): update auth repository - auth repository 클래스를 업데이트 합니다. - kakao 로그인 기능을 추가합니다. - naver 로그인 기능을 추가합니다. - 부적절한 반환값을 가진 메서드를의 반환값을 변경합니다. - 예외처리 로직들을 추가합니다. - 코드를 리팩터링 합니다. - 문서를 정리하여 추가합니다.

--- core/data/src/main/java/com/gradation/lift/data/repository/DefaultAuthRepository.kt
+++ core/data/src/main/java/com/gradation/lift/data/repository/DefaultAuthRepository.kt
@@ -7,14 +7,18 @@ import com.gradation.lift.domain.repository.AuthRepository
 import com.gradation.lift.model.model.auth.*
 import com.gradation.lift.network.common.NetworkResult
 import com.gradation.lift.network.datasource.auth.AuthDataSource
+import com.gradation.lift.oauth.kakao.KakaoOauthManager
+import com.gradation.lift.oauth.naver.NaverOauthManager
 import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
 class DefaultAuthRepository @Inject constructor(
     private val authDataSource: AuthDataSource,
     private val tokenDataStoreDataSource: TokenDataStoreDataSource,
+    private val kakaoOauthManager: KakaoOauthManager,
+    private val naverOauthManager: NaverOauthManager,
 ) : AuthRepository {
-    override fun signInDefault(signInInfo: DefaultSignInInfo): Flow<DataState<Boolean>> = flow {
+    override fun signInDefault(signInInfo: DefaultSignInInfo): Flow<DataState<Unit>> = flow {
         authDataSource.signInDefault(signInInfo).collect { result ->
             when (result) {
                 is NetworkResult.Fail -> emit(DataState.Fail(result.message))
@@ -22,24 +26,78 @@ class DefaultAuthRepository @Inject constructor(
                     tokenDataStoreDataSource.setAccessToken(result.data.accessToken)
                     tokenDataStoreDataSource.setRefreshToken(result.data.refreshToken)
                     tokenDataStoreDataSource.setLoginMethod(LoginMethod.Common())
-                    emit(DataState.Success(true))
+                    emit(DataState.Success(Unit))
                 }
             }
         }
     }
 
-    override fun signUpDefault(signUpInfo: DefaultSignUpInfo): Flow<DataState<Boolean>> = flow {
+    override fun signUpDefault(signUpInfo: DefaultSignUpInfo): Flow<DataState<Unit>> = flow {
         authDataSource.signUpDefault(signUpInfo).collect { result ->
             when (result) {
                 is NetworkResult.Fail -> emit(DataState.Fail(result.message))
                 is NetworkResult.Success -> {
-                    emit(DataState.Success(true))
+                    emit(DataState.Success(Unit))
                 }
             }
         }
     }
 
+    override fun signInNaver(): Flow<DataState<Unit>> = flow {
+        naverOauthManager.getUserId().collect { getUserIdResult ->
+            when (getUserIdResult) {
+                is DataState.Fail -> emit(DataState.Fail(getUserIdResult.message))
+                is DataState.Success -> {
+                    authDataSource.signInNaver(NaverSignInInfo(getUserIdResult.data))
+                        .collect { signInNaverResult ->
+                            when (signInNaverResult) {
+                                is NetworkResult.Fail -> emit(DataState.Fail(signInNaverResult.message))
+                                is NetworkResult.Success -> {
+                                    tokenDataStoreDataSource.setAccessToken(signInNaverResult.data.accessToken)
+                                    tokenDataStoreDataSource.setRefreshToken(signInNaverResult.data.refreshToken)
+                                    tokenDataStoreDataSource.setLoginMethod(LoginMethod.Naver())
+                                    emit(DataState.Success(Unit))
+                                }
+                            }
+                        }
+                }
+            }
 
+        }
+    }
+
+    override fun signInKakao(): Flow<DataState<Unit>> = flow {
+        kakaoOauthManager.getUserId().collect { getUserIdResult ->
+            when (getUserIdResult) {
+                is DataState.Fail -> emit(DataState.Fail(getUserIdResult.message))
+                is DataState.Success -> {
+                    authDataSource.signInKakao(KakaoSignInInfo(getUserIdResult.data))
+                        .collect { signInKakaoResult ->
+                            when (signInKakaoResult) {
+                                is NetworkResult.Fail -> emit(DataState.Fail(signInKakaoResult.message))
+                                is NetworkResult.Success -> {
+                                    tokenDataStoreDataSource.setAccessToken(signInKakaoResult.data.accessToken)
+                                    tokenDataStoreDataSource.setRefreshToken(signInKakaoResult.data.refreshToken)
+                                    tokenDataStoreDataSource.setLoginMethod(LoginMethod.Kakao())
+                                    emit(DataState.Success(Unit))
+                                }
+                            }
+                        }
+                }
+            }
+
+        }
+    }
+
+    override fun getLoginMethod(): Flow<DataState<LoginMethod>> = flow {
+        tokenDataStoreDataSource.loginMethod
+            .catch { it ->
+                DataState.Fail(it.toMessage())
+            }
+            .collect { loginMethod ->
+                emit(DataState.Success(loginMethod))
+            }
+    }
 
     override fun isSigned(): Flow<DataState<Boolean>> = flow {
         val condition = tokenDataStoreDataSource.accessToken.first().isNotBlank() &&
--- core/domain/src/main/java/com/gradation/lift/domain/repository/AuthRepository.kt
+++ core/domain/src/main/java/com/gradation/lift/domain/repository/AuthRepository.kt
@@ -3,12 +3,67 @@ package com.gradation.lift.domain.repository
 import com.gradation.lift.common.model.DataState
 import com.gradation.lift.model.model.auth.DefaultSignInInfo
 import com.gradation.lift.model.model.auth.DefaultSignUpInfo
+import com.gradation.lift.model.model.auth.LoginMethod
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * [AuthRepository]
+ * 사용자의 계정 및 토큰을 관리하는 레퍼지토리 클래스
+ * @since 2023-08-16 11:35:24
+ */
 interface AuthRepository {
-    fun signInDefault(signInInfo: DefaultSignInInfo): Flow<DataState<Boolean>>
-    fun signUpDefault(signUpInfo: DefaultSignUpInfo): Flow<DataState<Boolean>>
 
+    /**
+     * [signInDefault]
+     * 일반적인 방식으로 로그인 시 사용
+     * @param signInInfo 아이디와 패스워드를 저장하고 있는 클래스
+     * @since 2023-08-16 11:40:20
+     */
+    fun signInDefault(signInInfo: DefaultSignInInfo): Flow<DataState<Unit>>
+
+    /**
+     * [signUpDefault]
+     * 일반적인 방식으로 회원 가입 시 사용
+     * @param signUpInfo 회원가입 시에 필요한 데이터로 서버에 전달됨
+     * @since 2023-08-16 11:40:15
+     */
+    fun signUpDefault(signUpInfo: DefaultSignUpInfo): Flow<DataState<Unit>>
+
+    /**
+     * [signInNaver]
+     * 네이버를 통한 로그인 시 사용
+     * 서버 내에 회원 정보 저장하며 로그인을 동시에 진행˚
+     * @since 2023-08-16 11:40:09
+     */
+    fun signInNaver(): Flow<DataState<Unit>>
+
+    /**
+     * [signInKakao]
+     * 카카오를 통한 로그인 시 사용
+     * 서버 내에 회원 정보 저장하며 로그인을 동시에 진행
+     * @since 2023-08-16 11:40:03
+     */
+    fun signInKakao(): Flow<DataState<Unit>>
+
+    /**
+     * [getLoginMethod]
+     * 현재 어떠한 방식으로 로그인 했는지 확인
+     * @since 2023-08-16 11:39:55
+     */
+    fun getLoginMethod(): Flow<DataState<LoginMethod>>
+
+    /**
+     * [isSigned]
+     * 현재 로그인이 되어 있는 상태인지 확인
+     * @since 2023-08-16 11:39:49
+     */
     fun isSigned(): Flow<DataState<Boolean>>
+
+    /**
+     * [signOut]
+     * 로그아웃
+     * 클라이언트 기기에 저장되어있는 모든 토큰정보 삭제
+     * @since 2023-08-16 11:39:41
+     */
     fun signOut(): Flow<DataState<Unit>>
 }
