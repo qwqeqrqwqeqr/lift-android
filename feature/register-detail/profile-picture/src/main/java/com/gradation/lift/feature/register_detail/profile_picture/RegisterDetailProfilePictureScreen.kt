d38ba364ea611080665db3d94808e4bfc87a7724
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-08-18 14:22:16 +0900

    refactor(feature): refactor register-detail-profile-picture feature - profile-picture feature의 컴포넌트를을 분리합니다. - 전체적인 코드를 리팩터링합니다.

--- feature/register-detail/profile-picture/src/main/java/com/gradation/lift/feature/register_detail/profile_picture/RegisterDetailProfilePictureScreen.kt
+++ feature/register-detail/profile-picture/src/main/java/com/gradation/lift/feature/register_detail/profile_picture/RegisterDetailProfilePictureScreen.kt
@@ -2,40 +2,23 @@ package com.gradation.lift.feature.register_detail.profile_picture
 
 import android.annotation.SuppressLint
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
-import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
-import com.bumptech.glide.integration.compose.GlideImage
-import com.gradation.lift.designsystem.canvas.NumberCircle
 import com.gradation.lift.designsystem.component.LiftBackTopBar
-import com.gradation.lift.designsystem.component.LiftButton
+import com.gradation.lift.designsystem.component.LiftErrorSnackBar
 import com.gradation.lift.designsystem.theme.LiftMaterialTheme
 import com.gradation.lift.designsystem.theme.LiftTheme
 import com.gradation.lift.feature.register_detail.name.data.CreateUserDetailState
 import com.gradation.lift.feature.register_detail.name.data.RegisterDetailSharedViewModel
+import com.gradation.lift.feature.register_detail.profile_picture.component.*
 import com.gradation.lift.feature.register_detail.profile_picture.component.CompleteDialog
 import com.gradation.lift.feature.register_detail.profile_picture.data.RegisterDetailProfilePictureViewModel
 import com.gradation.lift.model.model.picture.UserProfilePicture
@@ -77,42 +60,92 @@ internal fun RegisterDetailProfilePictureRoute(
         sharedViewModel.updateCreateUserDetailState()
     val createUserDetail: () -> Unit = sharedViewModel.createUserDetail()
 
+    val snackbarHostState: SnackbarHostState by remember { mutableStateOf(SnackbarHostState()) }
+
     BackHandler(onBack = {
         updateCurrentRegisterProgressNumber(currentRegisterProgressNumber - 1)
         navigateProfilePictureToHeightWeight()
     })
 
+    when (val createUserDetailResult = createUserDetailState) {
+        is CreateUserDetailState.Fail -> {
+            LaunchedEffect(true) {
+                snackbarHostState.showSnackbar(
+                    message = createUserDetailResult.message, duration = SnackbarDuration.Short
+                )
+                updateCreateUserDetailState(CreateUserDetailState.None)
+            }
+        }
+        CreateUserDetailState.None -> {}
+        CreateUserDetailState.Success -> {
+            LaunchedEffect(true) {
+                updateOnVisibleCompleteDialog(true)
+            }
+        }
+    }
+
+    RegisterDetailProfilePictureScreen(
+        modifier,
+        selectedProfilePicture,
+        profilePictureList,
+        navigationCondition,
+        onVisibleCompleteDialog,
+        currentRegisterProgressNumber,
+        totalRegisterProgressNumber,
+        updateSelectedProfile,
+        updateCreateUserDetailProfilePicture,
+        updateCurrentRegisterProgressNumber,
+        createUserDetail,
+        navigateRegisterDetailGraphToHomeGraph,
+        navigateProfilePictureToHeightWeight,
+        snackbarHostState
+    )
+
 }
 
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalGlideComposeApi::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun RegisterDetailProfilePictureScreen(
     modifier: Modifier = Modifier,
-    onVisibleDialog: State<Boolean>,
-    profilePictureList: State<List<UserProfilePicture>>,
-    selectedProfile: State<String>,
-    navigationCondition: State<Boolean>,
-    onUpdateSelectedProfile: (String) -> Unit,
-    onClickCompleteDialogButton: () -> Unit,
-    onBackClickTopBar: () -> Unit,
-    onCompleteButtonClick: () -> Unit,
+    selectedProfilePicture: String,
+    profilePictureList: List<UserProfilePicture>,
+    navigationCondition: Boolean,
+    onVisibleCompleteDialog: Boolean,
+    currentRegisterProgressNumber: Int,
+    totalRegisterProgressNumber: Int,
+    updateSelectedProfile: (String) -> Unit,
+    updateCreateUserDetailProfilePicture: (String) -> Unit,
+    updateCurrentRegisterProgressNumber: (Int) -> Unit,
+    createUserDetail: () -> Unit,
+    navigateRegisterDetailGraphToHomeGraph: () -> Unit,
+    navigateProfilePictureToHeightWeight: () -> Unit,
+    snackbarHostState: SnackbarHostState,
 ) {
-    if (onVisibleDialog.value) {
+    if (onVisibleCompleteDialog) {
         Surface(
             color = LiftTheme.colorScheme.no23,
             modifier = modifier.fillMaxSize()
         ) {
-            CompleteDialog(onClickCompleteDialogButton = onClickCompleteDialogButton)
+            CompleteDialog(modifier, navigateRegisterDetailGraphToHomeGraph)
         }
     } else {
         Scaffold(
             topBar = {
                 LiftBackTopBar(
                     title = "추가정보 입력",
-                    onBackClickTopBar = onBackClickTopBar
+                    onBackClickTopBar = {
+                        updateCurrentRegisterProgressNumber(currentRegisterProgressNumber - 1)
+                        navigateProfilePictureToHeightWeight()
+                    }
                 )
             },
+            snackbarHost = {
+                LiftErrorSnackBar(
+                    modifier = modifier,
+                    snackbarHostState = snackbarHostState
+                )
+            }
         ) { padding ->
             Surface(
                 modifier = modifier
@@ -123,107 +156,32 @@ internal fun RegisterDetailProfilePictureScreen(
                 Column(
                     modifier = modifier
                         .padding(16.dp)
-                        .padding(padding)
                         .fillMaxSize()
                 ) {
-                    Row(horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.Start)) {
-                        repeat(5) {
-                            NumberCircle(number = it + 1, checked = it + 1 == 5)
-                        }
-
-                    }
-                    Spacer(modifier = modifier.padding(28.dp))
-                    Text(
-                        text = buildAnnotatedString {
-                            withStyle(
-                                style = SpanStyle(color = LiftTheme.colorScheme.no4),
-                            ) {
-                                append("프로필 사진")
-                            }
-                            append("을 등록해주세요")
-                        },
-                        style = LiftTheme.typography.no1,
-                        color = LiftTheme.colorScheme.no11,
+                    ProgressNumberView(
+                        modifier,
+                        currentRegisterProgressNumber,
+                        totalRegisterProgressNumber
                     )
+                    Spacer(modifier = modifier.padding(14.dp))
+                    HeaderView(modifier)
                     Spacer(modifier = modifier.padding(30.dp))
-
-
-                    if (selectedProfile.value.isEmpty()) {
-                        Box(
-                            modifier
-                                .clip(CircleShape)
-                                .size(96.dp)
-                                .background(
-                                    LiftTheme.colorScheme.no1
-                                )
-                                .align(Alignment.CenterHorizontally)
-                        )
-                    } else {
-                        GlideImage(
-                            model = selectedProfile.value,
-                            contentDescription = "",
-                            modifier = modifier
-                                .clip(CircleShape)
-                                .size(96.dp)
-                                .align(Alignment.CenterHorizontally)
-
-                        )
-                    }
-
+                    SelectedProfilePictureView(modifier, selectedProfilePicture)
                     Spacer(modifier = modifier.padding(13.dp))
-
-
-
-                    LazyVerticalGrid(
-                        columns = GridCells.Fixed(5),
-                        modifier = modifier.padding(16.dp),
-                        horizontalArrangement = Arrangement.spacedBy(8.dp),
-                    ) {
-                        items(profilePictureList.value) { item ->
-                            if (item.url == selectedProfile.value) {
-                                GlideImage(
-                                    model = item.url,
-                                    contentDescription = "",
-                                    modifier = modifier
-                                        .border(
-                                            width = 3.dp,
-                                            color = LiftTheme.colorScheme.no4,
-                                            shape = CircleShape
-                                        )
-                                        .clip(CircleShape)
-
-                                )
-                            } else {
-                                GlideImage(
-                                    model = item.url,
-                                    contentDescription = "",
-                                    modifier = modifier
-                                        .clip(CircleShape)
-                                        .clickable(
-                                            onClick = { onUpdateSelectedProfile(item.url) }
-                                        )
-                                )
-                            }
-
-                        }
-                    }
-
-
-
+                    ProfilePictureView(
+                        modifier,
+                        selectedProfilePicture,
+                        profilePictureList,
+                        updateSelectedProfile
+                    )
                     Spacer(modifier = modifier.padding(18.dp))
-
-
-                    LiftButton(
-                        modifier = modifier.fillMaxWidth(),
-                        onClick = onCompleteButtonClick,
-                        enabled = navigationCondition.value
-                    ) {
-                        Text(
-                            text = "완료",
-                            style = LiftTheme.typography.no3,
-                            color = LiftTheme.colorScheme.no5,
-                        )
-                    }
+                    NavigationView(
+                        modifier,
+                        selectedProfilePicture,
+                        navigationCondition,
+                        updateCreateUserDetailProfilePicture,
+                        createUserDetail
+                    )
                 }
             }
         }
@@ -237,22 +195,19 @@ internal fun RegisterDetailProfilePictureScreen(
 fun PreviewRegisterDetailProfilePictureScreen() {
     LiftMaterialTheme {
         RegisterDetailProfilePictureScreen(
-            onVisibleDialog = mutableStateOf(false),
-            profilePictureList = mutableStateOf(
-                listOf(
-                    UserProfilePicture(id = 1, url = "1"),
-                    UserProfilePicture(id = 1, url = "1"),
-                    UserProfilePicture(id = 1, url = "1"),
-                    UserProfilePicture(id = 1, url = "1"),
-                    UserProfilePicture(id = 1, url = "1"),
-                )
-            ),
-            selectedProfile = mutableStateOf(""),
-            navigationCondition = mutableStateOf(false),
-            onUpdateSelectedProfile = {},
-            onClickCompleteDialogButton = {},
-            onBackClickTopBar = {},
-            onCompleteButtonClick = {}
+            selectedProfilePicture = "",
+            profilePictureList = listOf(),
+            navigationCondition = true,
+            onVisibleCompleteDialog = false,
+            currentRegisterProgressNumber = 4,
+            totalRegisterProgressNumber = 4,
+            updateSelectedProfile = {},
+            updateCreateUserDetailProfilePicture = {},
+            updateCurrentRegisterProgressNumber = {},
+            createUserDetail = {},
+            navigateRegisterDetailGraphToHomeGraph = {},
+            navigateProfilePictureToHeightWeight = {},
+            snackbarHostState = SnackbarHostState()
         )
     }
 }
