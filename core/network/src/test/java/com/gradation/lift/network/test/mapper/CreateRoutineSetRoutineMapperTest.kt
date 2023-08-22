2720346e9619857de071e32e60a5a4397176198b
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-08-23 00:08:30 +0900

    fix(test): fix Mapper text error - CreateRoutineSetRoutineMapperTest.kt 에서 발생한 오류를 수정합니다.

--- core/network/src/test/java/com/gradation/lift/network/test/mapper/CreateRoutineSetRoutineMapperTest.kt
+++ core/network/src/test/java/com/gradation/lift/network/test/mapper/CreateRoutineSetRoutineMapperTest.kt
@@ -2,7 +2,7 @@ package com.gradation.lift.network.test.mapper
 
 import com.google.common.truth.Truth
 import com.gradation.lift.model.utils.ModelDataGenerator.RoutineSetRoutine.createRoutineSetRoutineModel
-import com.gradation.lift.network.data.TestDtoDataGenerator.RoutineSet.createRoutineSetRequestDto
+import com.gradation.lift.network.data.TestDtoDataGenerator.RoutineSet.createRoutineSetRoutineRequestDto
 import com.gradation.lift.network.mapper.toDto
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
@@ -16,12 +16,12 @@ class CreateRoutineSetRoutineMapperTest {
     fun testCreateRoutineSetRoutineMapper() = runTest {
 
         with(createRoutineSetRoutineModel.toDto()) {
-            Truth.assertThat(this.name).isEqualTo(createRoutineSetRequestDto.name)
-            Truth.assertThat(this.description).isEqualTo(createRoutineSetRequestDto.description)
-            Truth.assertThat(this.weekday).isEqualTo(createRoutineSetRequestDto.weekday)
-            Truth.assertThat(this.picture).isEqualTo(createRoutineSetRequestDto.picture)
+            Truth.assertThat(this.name).isEqualTo(createRoutineSetRoutineRequestDto.name)
+            Truth.assertThat(this.description).isEqualTo(createRoutineSetRoutineRequestDto.description)
+            Truth.assertThat(this.weekday).isEqualTo(createRoutineSetRoutineRequestDto.weekday)
+            Truth.assertThat(this.picture).isEqualTo(createRoutineSetRoutineRequestDto.picture)
             Truth.assertThat(this.routine)
-                .isEqualTo(createRoutineSetRequestDto.routine)
+                .isEqualTo(createRoutineSetRoutineRequestDto.routine)
         }
     }
 }
