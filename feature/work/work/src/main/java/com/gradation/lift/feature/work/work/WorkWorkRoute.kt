414c2b1ead0755d8c7749ec5c21ca92d25175565
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-08-22 17:26:54 +0900

    feat(feature): update work-work feature - work-work feature의 화면 레이아웃을 조정합니다

--- feature/work/work/src/main/java/com/gradation/lift/feature/work/work/WorkWorkRoute.kt
+++ feature/work/work/src/main/java/com/gradation/lift/feature/work/work/WorkWorkRoute.kt
@@ -49,7 +49,7 @@ fun WorkWorkRoute(
     val autoCompleteState: Boolean by viewModel.autoCompleteState.collectAsStateWithLifecycle()
 
     val updateScreenState: (WorkScreenUiState) -> Unit = viewModel.updateWorkScreenState()
-    val updateDialogState: (WorkDialogUiState)-> Unit = viewModel.updateWorkDialogState()
+    val updateDialogState: (WorkDialogUiState) -> Unit = viewModel.updateWorkDialogState()
     val updateWorkState: (Boolean) -> Unit = sharedViewModel.workState.updateWorkState()
     val offAutoCompleteState: () -> Unit = viewModel.offAutoCompleteState()
 
@@ -63,12 +63,17 @@ fun WorkWorkRoute(
     val workTime: WorkRestTime by sharedViewModel.workState.workRestTime.collectAsStateWithLifecycle()
     val workProgress: Int by sharedViewModel.workState.workProgress.collectAsStateWithLifecycle()
 
-    val updateOpenedWorkRoutine: (WorkRoutineSelection)-> Unit = sharedViewModel.workState.updateOpenedWorkRoutine()
-    val updateCheckedWorkSet: (Pair<Int,Int>,Boolean) -> Unit = sharedViewModel.workState.updateCheckedWorkSet()
-    val updateWorkIndexToPreviousIndex: () -> Unit = sharedViewModel.workState.updateWorkIndexToPreviousIndex()
-    val updateWorkIndexToNextIndex: () -> Unit = sharedViewModel.workState.updateWorkIndexToNextIndex()
+    val updateOpenedWorkRoutine: (WorkRoutineSelection) -> Unit =
+        sharedViewModel.workState.updateOpenedWorkRoutine()
+    val updateCheckedWorkSet: (Pair<Int, Int>, Boolean) -> Unit =
+        sharedViewModel.workState.updateCheckedWorkSet()
+    val updateWorkIndexToPreviousIndex: () -> Unit =
+        sharedViewModel.workState.updateWorkIndexToPreviousIndex()
+    val updateWorkIndexToNextIndex: () -> Unit =
+        sharedViewModel.workState.updateWorkIndexToNextIndex()
 
-    val isAllCheckedWorkSet: (List<WorkSetSelection>) -> (Boolean) = sharedViewModel.isAllCheckedWorkSet()
+    val isAllCheckedWorkSet: (List<WorkSetSelection>) -> (Boolean) =
+        sharedViewModel.isAllCheckedWorkSet()
 
 
     BackHandler(enabled = true, onBack = { updateDialogState(WorkDialogUiState.SuspendDialogUi) })
@@ -80,91 +85,89 @@ fun WorkWorkRoute(
         offAutoCompleteState()
         updateDialogState(WorkDialogUiState.CompleteDialogUi)
     }
-
-    when (workDialogUiState) {
-        WorkDialogUiState.AutoCompleteDialogUi -> {
-            Surface(
-                color = LiftTheme.colorScheme.no23, modifier = modifier.fillMaxSize()
-            ) {
-                AutoCompleteDialog(
-                    onClickDialogCompleteButton = navigateWorkToComplete,
-                    onClickDialogDismissButton = { updateDialogState(WorkDialogUiState.None) },
-                )
-            }
-        }
-
-        WorkDialogUiState.SuspendDialogUi -> {
-            Surface(
-                color = LiftTheme.colorScheme.no23, modifier = modifier.fillMaxSize()
-            ) {
-                SuspendDialog(
-                    onClickDialogSuspendButton = navigateWorkGraphToHomeGraph,
-                    onClickDialogDismissButton = { updateDialogState(WorkDialogUiState.None) },
-                )
-            }
-        }
-        WorkDialogUiState.CompleteDialogUi -> {
-            Surface(
-                color = LiftTheme.colorScheme.no23, modifier = modifier.fillMaxSize()
-            ) {
-                CompleteDialog(
-                    completeState = workProgress == MAX_PROGRESS,
-                    onClickDialogCompleteButton = navigateWorkToComplete,
-                    onClickDialogDismissButton = { updateDialogState(WorkDialogUiState.None) },
-                )
-            }
-        }
-        WorkDialogUiState.None -> {
-            when (workScreenUiState) {
-                is WorkScreenUiState.ListScreenUi -> {
-                    WorkListScreen(
-                        modifier = modifier,
-                        onCloseClickTopBar = { updateScreenState(WorkScreenUiState.WorkScreenUi) },
-                        workTime = workTime,
-                        workProgress = workProgress,
-                        workList = workList,
-                        updateOpenedWorkRoutine = updateOpenedWorkRoutine,
-                        updateCheckedWorkSet = updateCheckedWorkSet,
-                        isAllCheckedWorkSet = isAllCheckedWorkSet
+    Box{
+        when (workDialogUiState) {
+            WorkDialogUiState.AutoCompleteDialogUi -> {
+                Surface(
+                    color = LiftTheme.colorScheme.no4.copy(alpha = 0.7f), modifier = modifier.fillMaxSize()
+                ) {
+                    AutoCompleteDialog(
+                        onClickDialogCompleteButton = navigateWorkToComplete,
+                        onClickDialogDismissButton = { updateDialogState(WorkDialogUiState.None) },
                     )
                 }
-                WorkScreenUiState.RestScreenUi -> {
-                    WorkRestScreen(
-                        modifier = modifier,
-                        onCloseClickTopBar = { updateDialogState(WorkDialogUiState.SuspendDialogUi) },
-                        onListClickTopBar = { updateScreenState(WorkScreenUiState.ListScreenUi(false)) },
-                        onClickWorkCompleteButton = { updateDialogState(WorkDialogUiState.CompleteDialogUi) },
-                        onClickWorkButton = { updateScreenState(WorkScreenUiState.WorkScreenUi) },
-                        updateWorkState = { updateWorkState(true) },
-                        workTime = workTime,
-                        workProgress = workProgress,
-                        currentWork = currentWork
-                    )
-                }
-                WorkScreenUiState.WorkScreenUi -> {
-                    WorkWorkScreen(
-                        modifier = modifier,
-                        onCloseClickTopBar = { updateDialogState(WorkDialogUiState.SuspendDialogUi) },
-                        onListClickTopBar = { updateScreenState(WorkScreenUiState.ListScreenUi(true)) },
-                        onClickWorkCompleteButton = { updateDialogState(WorkDialogUiState.CompleteDialogUi) },
-                        onClickRestButton = { updateScreenState(WorkScreenUiState.RestScreenUi) },
-                        updateWorkState = { updateWorkState(false) },
-                        updateCheckedWorkSet = updateCheckedWorkSet,
-                        updateWorkIndexToPreviousIndex = updateWorkIndexToPreviousIndex,
-                        updateWorkIndexToNextIndex = updateWorkIndexToNextIndex,
+            }
 
-                        workTime = workTime,
-                        workProgress = workProgress,
-                        currentWork = currentWork,
-                        previousWork = previousWork,
-                        nextWork = nextWork,
+            WorkDialogUiState.SuspendDialogUi -> {
+                Surface(
+                    color = LiftTheme.colorScheme.no4.copy(alpha = 0.7f), modifier = modifier.fillMaxSize()
+                ) {
+                    SuspendDialog(
+                        onClickDialogSuspendButton = navigateWorkGraphToHomeGraph,
+                        onClickDialogDismissButton = { updateDialogState(WorkDialogUiState.None) },
                     )
                 }
+            }
+            WorkDialogUiState.CompleteDialogUi -> {
+                Surface(
+                    color = LiftTheme.colorScheme.no4.copy(alpha = 0.7f),  modifier = modifier.fillMaxSize()
+                ) {
+                    CompleteDialog(
+                        completeState = workProgress == MAX_PROGRESS,
+                        onClickDialogCompleteButton = navigateWorkToComplete,
+                        onClickDialogDismissButton = { updateDialogState(WorkDialogUiState.None) },
+                    )
+                }
+            }
+            WorkDialogUiState.None -> {}
+        }
+        when (workScreenUiState) {
+            is WorkScreenUiState.ListScreenUi -> {
+                WorkListScreen(
+                    modifier = modifier,
+                    onCloseClickTopBar = { updateScreenState(WorkScreenUiState.WorkScreenUi) },
+                    workTime = workTime,
+                    workProgress = workProgress,
+                    workList = workList,
+                    updateOpenedWorkRoutine = updateOpenedWorkRoutine,
+                    updateCheckedWorkSet = updateCheckedWorkSet,
+                    isAllCheckedWorkSet = isAllCheckedWorkSet
+                )
+            }
+            WorkScreenUiState.RestScreenUi -> {
+                WorkRestScreen(
+                    modifier = modifier,
+                    onCloseClickTopBar = { updateDialogState(WorkDialogUiState.SuspendDialogUi) },
+                    onListClickTopBar = { updateScreenState(WorkScreenUiState.ListScreenUi(false)) },
+                    onClickWorkCompleteButton = { updateDialogState(WorkDialogUiState.CompleteDialogUi) },
+                    onClickWorkButton = { updateScreenState(WorkScreenUiState.WorkScreenUi) },
+                    updateWorkState = { updateWorkState(true) },
+                    workTime = workTime,
+                    workProgress = workProgress,
+                    currentWork = currentWork
+                )
+            }
+            WorkScreenUiState.WorkScreenUi -> {
+                WorkWorkScreen(
+                    modifier = modifier,
+                    onCloseClickTopBar = { updateDialogState(WorkDialogUiState.SuspendDialogUi) },
+                    onListClickTopBar = { updateScreenState(WorkScreenUiState.ListScreenUi(true)) },
+                    onClickWorkCompleteButton = { updateDialogState(WorkDialogUiState.CompleteDialogUi) },
+                    onClickRestButton = { updateScreenState(WorkScreenUiState.RestScreenUi) },
+                    updateWorkState = { updateWorkState(false) },
+                    updateCheckedWorkSet = updateCheckedWorkSet,
+                    updateWorkIndexToPreviousIndex = updateWorkIndexToPreviousIndex,
+                    updateWorkIndexToNextIndex = updateWorkIndexToNextIndex,
+
+                    workTime = workTime,
+                    workProgress = workProgress,
+                    currentWork = currentWork,
+                    previousWork = previousWork,
+                    nextWork = nextWork,
+                )
             }
         }
     }
-
-
 }
 
 
