5677e6347dde4c5cb60712d98615047b073e10ed
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-08-28 20:07:16 +0900

    refactor(data) : refactor repository - repository 인터페이스를 전체적으로 리팩터링합니다. - repository 인터페이스의 기능들을 문서화합니다.

--- core/domain/src/main/java/com/gradation/lift/domain/repository/CheckerRepository.kt
+++ core/domain/src/main/java/com/gradation/lift/domain/repository/CheckerRepository.kt
@@ -3,9 +3,24 @@ package com.gradation.lift.domain.repository
 import com.gradation.lift.common.model.DataState
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * [CheckerRepository]
+ * 유효성을 검증하는 저장소
+ * @since 2023-08-28 17:01:32
+ */
 interface CheckerRepository {
 
+    /**
+     * [checkDuplicateEmail]
+     *  입력 받은 이메일과 동일한 이메일이 존재하는지 확인
+     *  @since 2023-08-28 17:02:16
+     */
     fun checkDuplicateEmail(email: String): Flow<DataState<Boolean>>
 
+    /**
+     * [checkDuplicateName]
+     * 입력 받은 이름(닉네임)과 동일한 이름(닉네임)이 존재하는지 확인
+     * @since 2023-08-28 17:02:47
+     */
     fun checkDuplicateName(name: String): Flow<DataState<Boolean>>
 }
--- core/domain/src/main/java/com/gradation/lift/domain/repository/UserRepository.kt
+++ core/domain/src/main/java/com/gradation/lift/domain/repository/UserRepository.kt
@@ -4,14 +4,40 @@ import com.gradation.lift.common.model.DataState
 import com.gradation.lift.model.model.user.UserDetail
 import kotlinx.coroutines.flow.Flow
 
+
+/**
+ *  [UserRepository]
+ *  사용자 정보를 관리하는 저장소
+ *  @since 2023-08-28 20:00:06
+ */
 interface UserRepository {
 
+     /**
+      * [getUserDetail]
+      * 사용자의 상세정보를 불러옴
+      * @since 2023-08-28 20:00:19
+      */
      fun getUserDetail(): Flow<DataState<UserDetail>>
 
+     /**
+      * [createUserDetail]
+      * 사용자의 상세정보를 생성함
+      * @since 2023-08-28 20:00:34
+      */
      fun createUserDetail(userDetail: UserDetail): Flow<DataState<Boolean>>
 
+     /**
+      * [updateUserDetail]
+      * 사용자의 상세정보를 갱신함
+      * @since 2023-08-28 20:00:59
+      */
      fun updateUserDetail(userDetail: UserDetail): Flow<DataState<Boolean>>
 
+     /**
+      * [existUserDetail]
+      * 사용자의 상세정보가 존재하는지 확인
+      * 조건에 따라 상세정보 페이지로 navigate 하기 위해 필요
+      */
      fun existUserDetail(): Flow<DataState<Boolean>>
 
 }
