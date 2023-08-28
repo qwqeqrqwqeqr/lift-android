9ad302f457467b43cd33e50f46c8e1325327ea3e
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-08-28 22:49:28 +0900

    refactor(common) : refactor common module - common 모듈 중 불필요한 코드들을 제거합니다. - 코드들의 스타일 간격을 조정합니다.

--- core/common/src/main/java/com/gradation/lift/common/model/DataState.kt
+++ core/common/src/main/java/com/gradation/lift/common/model/DataState.kt
@@ -1,8 +1,14 @@
 package com.gradation.lift.common.model
 
-sealed class DataState<out T> {
-    data class Success<out T>(val data: T) : DataState<T>()
-    data class Fail(val message: String) : DataState<Nothing>()
+/**
+ * [DataState]
+ * 계층 간 이동 시에 사용되는 상태
+ * 해당 상태를 기준으로 계층간 통신이 이루어짐
+ * @since 2023-08-28 22:48:35
+ */
+sealed interface DataState<out T> {
+    data class Success<out T>(val data: T) : DataState<T>
+    data class Fail(val message: String) : DataState<Nothing>
 }
 
 
