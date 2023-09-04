e79beee3fb1f9d585c7496e0a2668fc58abf3727
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-09-04 19:57:17 +0900

    feat(feature): feat hexagon chart - 운동 부위 별 횟수를 표현할 차트인 hexagon chart 를 제작합니다.

--- core/designsystem/src/main/java/com/gradation/lift/designsystem/chart/model/HexagonChartItem.kt
+++ core/designsystem/src/main/java/com/gradation/lift/designsystem/chart/model/HexagonChartItem.kt
@@ -1,13 +1,6 @@
 package com.gradation.lift.designsystem.chart.model
 
 data class WorkHexagonChartItem(
-    private val chest : String = "가슴",
-    private val shoulder : String = "어꺠",
-    private val arm : String = "팔",
-    private val back : String = "등",
-    private val lowerBody : String = "하체",
-    private  val abs : String = "복근",
-
     val chestValue : Int = 0,
     val shoulderValue : Int = 0,
     val armValue : Int = 0,
