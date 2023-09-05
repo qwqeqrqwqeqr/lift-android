43d1bb5fa92a41b4bbd5c0f0262883c91c0d1987
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-09-05 19:53:53 +0900

    feat(feature): feat history-daily-log feature - history-daily-log feature 생성합니다 - history-daily-log feature 의 viewmodel을 생성하고 메서드및 필드를 추가합니다 - viewmodel에서 사용될 model 과 state를 정의합니다 - historyanalytics viewmodel의 일부 네이밍 및 문서를 리팩터링합니다 - calander 이미지를 추가합니다.

--- feature/history/daily-log/src/main/java/com/gradation/lift/feature/history/daily_log/HistoryDailyLogScreen.kt
+++ feature/history/daily-log/src/main/java/com/gradation/lift/feature/history/daily_log/HistoryDailyLogScreen.kt
@@ -1,25 +1,59 @@
 package com.gradation.lift.feature.history.daily_log
 
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.gradation.lift.designsystem.theme.LiftMaterialTheme
 import com.gradation.lift.designsystem.theme.LiftTheme
+import com.gradation.lift.feature.history.daily_log.data.HistoryDailyLogViewModel
 
 @Composable
 fun HistoryDailyLogRoute(
-    modifier: Modifier =Modifier,
+    modifier: Modifier = Modifier,
     viewModel: HistoryDailyLogViewModel = hiltViewModel(),
-){
+) {
 
-    HistoryDailyLogScreen(modifier)
+
+    val scrollState: ScrollState = rememberScrollState()
+
+
+    HistoryDailyLogScreen(modifier, scrollState)
 
 }
 
 @Composable
-fun HistoryDailyLogScreen(modifier:Modifier=Modifier){
-    Surface(color = LiftTheme.colorScheme.no5, modifier = modifier.fillMaxSize()) {
+fun HistoryDailyLogScreen(
+    modifier: Modifier = Modifier,
+    scrollState: ScrollState,
+) {
+    Surface(color = LiftTheme.colorScheme.no17, modifier = modifier.fillMaxSize()) {
 
+        Column(
+            modifier = modifier
+                .padding(top = 16.dp)
+                .verticalScroll(scrollState)
+        ) {
+
+        }
+    }
+}
+
+@Composable
+@Preview
+fun HistoryDailyLogScreenPreview() {
+    LiftMaterialTheme {
+        HistoryDailyLogScreen(
+            Modifier,
+            rememberScrollState()
+        )
     }
 }
---
+++ feature/history/daily-log/src/main/java/com/gradation/lift/feature/history/daily_log/data/HistoryDailyLogViewModel.kt
@@ -0,0 +1,102 @@
+package com.gradation.lift.feature.history.daily_log.data
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.gradation.lift.common.model.DataState
+import com.gradation.lift.domain.usecase.date.GetTodayUseCase
+import com.gradation.lift.domain.usecase.date.GetWeekDateOfCurrentMonthUseCase
+import com.gradation.lift.domain.usecase.history.GetHistoryUseCase
+import com.gradation.lift.feature.history.daily_log.data.model.HistoryScoreWeekDate
+import com.gradation.lift.feature.history.daily_log.data.state.HistoryUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.datetime.DatePeriod
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.minus
+import kotlinx.datetime.plus
+import javax.inject.Inject
+
+/**
+ * [HistoryDailyLogViewModel]
+ * @property selectedDate 선택된 날짜, 해당 날짜를 바탕으로 히스토리 정보를 가져옴
+ * @property historyUiState 운동 기록 관련 상태
+ * @property selectedHistory 현재 선택된 날짜에 대한 히스토리
+ * @property historyScoreByMonth 한달 운동 기록에 대한 운동기록 점수
+ * @since 2023-09-05 19:51:04
+ */
+@HiltViewModel
+class HistoryDailyLogViewModel @Inject constructor(
+    getHistoryUseCase: GetHistoryUseCase,
+    getTodayUseCase: GetTodayUseCase,
+    getWeekDateOfCurrentMonthUseCase: GetWeekDateOfCurrentMonthUseCase,
+) : ViewModel() {
+
+    val selectedDate: MutableStateFlow<LocalDate> = MutableStateFlow(getTodayUseCase())
+
+    val historyUiState: StateFlow<HistoryUiState> = getHistoryUseCase().map {
+        when (val getHistoryResult = it) {
+            is DataState.Fail -> HistoryUiState.Fail(getHistoryResult.message)
+            is DataState.Success -> {
+                with(getHistoryResult.data) {
+                    if (isEmpty()) HistoryUiState.Empty else HistoryUiState.Success(toList())
+                }
+            }
+        }
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000),
+        initialValue = HistoryUiState.None
+    )
+    val selectedHistory =
+        combine(selectedDate, historyUiState) { selectedDate, historyUiStateResult ->
+            if (historyUiStateResult is HistoryUiState.Success) {
+                historyUiStateResult.historyList.filter { it.historyTimeStamp.date == selectedDate }
+            } else emptyList()
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = emptyList()
+        )
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    val historyScoreByMonth: StateFlow<List<HistoryScoreWeekDate>> =
+        combine(selectedDate, historyUiState) { selectedMonth, historyUiStateResult ->
+            if (historyUiStateResult is HistoryUiState.Success) {
+                getWeekDateOfCurrentMonthUseCase(selectedMonth).map { weekDateMonth ->
+                    HistoryScoreWeekDate(
+                        score = historyUiStateResult.historyList.filter {
+                            it.historyTimeStamp.date == weekDateMonth.date
+                        }.sortedByDescending { it.historyTimeStamp }.first().score,
+                        week = weekDateMonth.week,
+                        weekday = weekDateMonth.weekday,
+                        date = weekDateMonth.date
+                    )
+                }
+            } else emptyList()
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = emptyList()
+        )
+
+
+    fun minusMonthSelectedDate(): () -> Unit = {
+        selectedDate.value = selectedDate.value.minus(DatePeriod(0, 1, 0))
+    }
+
+    fun plusMonthSelectedDate(): () -> Unit = {
+        selectedDate.value = selectedDate.value.plus(DatePeriod(0, 1, 0))
+    }
+
+    fun updateSelectedDate(): (LocalDate) -> Unit = {
+        selectedDate.value = it
+    }
+
+}
