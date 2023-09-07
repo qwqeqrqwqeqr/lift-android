b39621b22a056a7c8c5d1103ed4179be317fa864
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-09-07 15:18:22 +0900

    feat(navigation): update update-routine-routine-set feature
    - routine-set feature 를 업데이트 합니다.
    - viewmodel, navigation 및 sharedviewmodel에서 사용되는 데이터들을 모두 뷰에 정의합니다.

--- feature/update-routine/routine-set/src/main/java/com/gradation/lift/feature/update_routine/routine_set/UpdateRoutineRoutineSetNavigation.kt
+++ feature/update-routine/routine-set/src/main/java/com/gradation/lift/feature/update_routine/routine_set/UpdateRoutineRoutineSetNavigation.kt
@@ -4,6 +4,8 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.gradation.lift.navigation.Router.UPDATE_ROUTINE_ROUTINE_SET_ROUTER_NAME
+import com.gradation.lift.navigation.navigation.navigateRoutineSetToFindWorkCategoryInUpdateRoutineGraph
+import com.gradation.lift.navigation.navigation.navigateRoutineSetToProfilePictureInUpdateRoutineGraph
 import com.gradation.lift.navigation.navigation.navigateRoutineSetToRoutineSelectionInUpdateRoutineGraph
 
 fun updateRoutineRoutineSetScreen(
@@ -14,12 +16,17 @@ fun updateRoutineRoutineSetScreen(
         val navigateRoutineSetToRoutineSelectionInUpdateRoutineGraph: () -> Unit =
             { navController.navigateRoutineSetToRoutineSelectionInUpdateRoutineGraph() }
 
+        val navigateRoutineSetToFindWorkCategoryInUpdateRoutineGraph: () -> Unit =
+            { navController.navigateRoutineSetToFindWorkCategoryInUpdateRoutineGraph() }
 
-
+        val navigateRoutineSetToProfilePictureInUpdateRoutineGraph: () -> Unit =
+            { navController.navigateRoutineSetToProfilePictureInUpdateRoutineGraph() }
 
         UpdateRoutineRoutineSetRoute(
             navController,
-            navigateRoutineSetToRoutineSelectionInUpdateRoutineGraph
+            navigateRoutineSetToRoutineSelectionInUpdateRoutineGraph,
+            navigateRoutineSetToFindWorkCategoryInUpdateRoutineGraph,
+            navigateRoutineSetToProfilePictureInUpdateRoutineGraph
         )
     }
 }
--- feature/update-routine/routine-set/src/main/java/com/gradation/lift/feature/update_routine/routine_set/UpdateRoutineRoutineSetScreen.kt
+++ feature/update-routine/routine-set/src/main/java/com/gradation/lift/feature/update_routine/routine_set/UpdateRoutineRoutineSetScreen.kt
@@ -1,8 +1,15 @@
 package com.gradation.lift.feature.update_routine.routine_set
 
 import android.annotation.SuppressLint
+import android.os.Build
+import androidx.activity.compose.BackHandler
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -15,15 +22,22 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
+import com.gradation.lift.designsystem.component.LiftBackTopBar
+import com.gradation.lift.designsystem.theme.LiftTheme
 import com.gradation.lift.feature.update_routine.routine_set.data.UpdateRoutineState
 import com.gradation.lift.feature.update_routine.routine_selection.data.UpdateRoutineSharedViewModel
+import com.gradation.lift.model.model.routine.UpdateRoutine
+import com.gradation.lift.model.model.routine.UpdateRoutineSetRoutine
 import com.gradation.lift.navigation.Router
 
+@RequiresApi(Build.VERSION_CODES.O)
 @SuppressLint("UnrememberedGetBackStackEntry")
 @Composable
 fun UpdateRoutineRoutineSetRoute(
     navController: NavController,
     navigateRoutineSetToRoutineSelectionInUpdateRoutineGraph: () -> Unit,
+    navigateRoutineSetToFindWorkCategoryInUpdateRoutineGraph: () -> Unit,
+    navigateRoutineSetToProfilePictureInUpdateRoutineGraph: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: UpdateRoutineRoutineSetViewModel = hiltViewModel(),
 ) {
@@ -31,18 +45,22 @@ fun UpdateRoutineRoutineSetRoute(
         remember { navController.getBackStackEntry(Router.UPDATE_ROUTINE_GRAPH_NAME) }
     val sharedViewModel: UpdateRoutineSharedViewModel = hiltViewModel(workBackStackEntry)
 
-    val updateRoutineState : UpdateRoutineState by viewModel.updateRoutineState.collectAsStateWithLifecycle()
-
-    val updateUpdateRoutineState :(UpdateRoutineState) -> Unit = viewModel.updateUpdateRoutineState()
-
-    UpdateRoutineRoutineSetScreen(
-        modifier = modifier
-    )
+    val updateRoutineState: UpdateRoutineState by viewModel.updateRoutineState.collectAsStateWithLifecycle()
+    val selectedRoutineSetRoutine: UpdateRoutineSetRoutine by sharedViewModel.selectedRoutineSetRoutine.collectAsStateWithLifecycle()
 
 
+    val deleteRoutineSetRoutine: (Int) -> Unit = viewModel.deleteRoutineSetRoutine()
+    val updateRoutineSetRoutine: (UpdateRoutineSetRoutine) -> Unit = viewModel.updateRoutineSetRoutine()
 
 
-    val snackbarHostState by remember { mutableStateOf(SnackbarHostState()) }
+    val updateUpdateRoutineState: (UpdateRoutineState) -> Unit =
+        viewModel.updateUpdateRoutineState()
+    val updateSelectedRoutineSetRoutine: (UpdateRoutineSetRoutine) -> Unit =
+        sharedViewModel.updateSelectedRoutineSetRoutine()
+    val removeRoutine: (UpdateRoutine) -> Unit = sharedViewModel.removeRoutine()
+
+
+    val snackbarHostState: SnackbarHostState by remember { mutableStateOf(SnackbarHostState()) }
     val focusManager: FocusManager = LocalFocusManager.current
 
 
@@ -55,6 +73,7 @@ fun UpdateRoutineRoutineSetRoute(
                 updateUpdateRoutineState(UpdateRoutineState.None)
             }
         }
+
         UpdateRoutineState.None -> {}
         is UpdateRoutineState.Success -> {
             LaunchedEffect(true) {
@@ -62,6 +81,10 @@ fun UpdateRoutineRoutineSetRoute(
             }
         }
     }
+
+    BackHandler(enabled = true, onBack = navigateRoutineSetToRoutineSelectionInUpdateRoutineGraph)
+
+
 }
 
 
@@ -69,6 +92,22 @@ fun UpdateRoutineRoutineSetRoute(
 internal fun UpdateRoutineRoutineSetScreen(
     modifier: Modifier = Modifier,
 ) {
+    Scaffold(
+        topBar = {
+            LiftBackTopBar(
+                title = "루틴리스트 수정",
+                onBackClickTopBar = {}
+            )
+        }
+    ) {
+        Surface(
+            color = LiftTheme.colorScheme.no17,
+            modifier = modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
 
+        }
+    }
 
 }
