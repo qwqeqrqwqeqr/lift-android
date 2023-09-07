e21a9d1951575b52674eff08f962580dfc41cf57
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-09-07 09:09:23 +0900

    feat(feature): update update-routine-routine feature - update-routine-routine feature를 업데이트합니다. - 루틴 업데이트와 관련한 상태를 추가합니다. - 해당 feature의 네비게이션을 추가하고 연동합니다.

--- feature/update-routine/routine/src/main/java/com/gradation/lift/feature/update_routine/routine/UpdateRoutineRoutineScreen.kt
+++ feature/update-routine/routine/src/main/java/com/gradation/lift/feature/update_routine/routine/UpdateRoutineRoutineScreen.kt
@@ -1,21 +1,57 @@
 package com.gradation.lift.feature.update_routine.routine
 
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
+import com.gradation.lift.feature.update_routine.routine.data.UpdateRoutineState
 
 @Composable
 fun UpdateRoutineRoutineRoute(
     navController: NavController,
+    navigateRoutineToRoutineSelection: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: UpdateRoutineRoutineViewModel = hiltViewModel(),
 ) {
 
+    val updateRoutineState : UpdateRoutineState by viewModel.updateRoutineState.collectAsStateWithLifecycle()
+
+    val updateUpdateRoutineState :(UpdateRoutineState) -> Unit = viewModel.updateUpdateRoutineState()
 
     UpdateRoutineRoutineScreen(
         modifier = modifier
     )
+
+    val snackbarHostState by remember { mutableStateOf(SnackbarHostState()) }
+    val focusManager: FocusManager = LocalFocusManager.current
+
+
+
+    when (val updateRoutineStateResult = updateRoutineState) {
+        is UpdateRoutineState.Fail -> {
+            LaunchedEffect(true) {
+                snackbarHostState.showSnackbar(
+                    message = updateRoutineStateResult.message, duration = SnackbarDuration.Short
+                )
+                updateUpdateRoutineState(UpdateRoutineState.None)
+            }
+        }
+        UpdateRoutineState.None -> {}
+        is UpdateRoutineState.Success -> {
+            LaunchedEffect(true) {
+                navigateRoutineToRoutineSelection()
+            }
+        }
+    }
 }
 
 
--- feature/update-routine/routine/src/main/java/com/gradation/lift/feature/update_routine/routine/UpdateRoutineRoutineViewModel.kt
+++ feature/update-routine/routine/src/main/java/com/gradation/lift/feature/update_routine/routine/UpdateRoutineRoutineViewModel.kt
@@ -1,14 +1,23 @@
 package com.gradation.lift.feature.update_routine.routine
 
 import androidx.lifecycle.ViewModel
+import com.gradation.lift.feature.update_routine.routine.data.UpdateRoutineState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class UpdateRoutineRoutineViewModel  @Inject constructor(
+class UpdateRoutineRoutineViewModel @Inject constructor(
 
-): ViewModel() {
+) : ViewModel() {
 
+    var updateRoutineState: MutableStateFlow<UpdateRoutineState> =
+        MutableStateFlow(UpdateRoutineState.None)
+
+
+    fun updateUpdateRoutineState(): (UpdateRoutineState) -> Unit = {
+        updateRoutineState.value = it
+    }
 
 
 }
