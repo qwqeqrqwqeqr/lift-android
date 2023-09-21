3649706c5d9201b22cbb64c04d2073645fa0df11
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-09-21 15:03:03 +0900

    feat(network): feat datasource - badge datasource를 추가합니다. - notification datasource를 추가합니다. - di모듈들을 생성하고 연동합니다. - BadgeCondition 모델을 추가합니다.

--- core/network/src/main/java/com/gradation/lift/network/dto/badge/GetUserBadgeConditionResponseDto.kt
+++ core/network/src/main/java/com/gradation/lift/network/dto/badge/GetUserBadgeConditionResponseDto.kt
@@ -1,6 +1,7 @@
 package com.gradation.lift.network.dto.badge
 
 import com.gradation.lift.model.model.badge.Badge
+import com.gradation.lift.model.model.badge.BadgeCondition
 import com.gradation.lift.network.common.Constants
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
@@ -11,14 +12,16 @@ data class GetUserBadgeConditionResponseDto(
     @Json(name = "badge")
     val badge: BadgeDto? = null
 ) {
-    fun toDomain(): Badge? =
-        badge?.let {
-            Badge(
-                id = it.id,
-                name = badge.name,
-                description = badge.description,
-                hint = badge.hint,
-                url = Constants.DEFAULT_S3_URL + badge.url
-            )
-        }
+    fun toDomain(): BadgeCondition =
+        BadgeCondition(
+            badge?.let {
+                Badge(
+                    id = it.id,
+                    name = badge.name,
+                    description = badge.description,
+                    hint = badge.hint,
+                    url = Constants.DEFAULT_S3_URL + badge.url
+                )
+            }
+        )
 }
