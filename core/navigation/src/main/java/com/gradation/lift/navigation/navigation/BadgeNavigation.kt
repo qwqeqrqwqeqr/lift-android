8c17629efde6e58c3d4a3a68645e683af1e532ce
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-09-30 13:53:00 +0900

    feat(navigation): update navigation method - 기존 뱃지화면에서 사용되던 이전화면으로 돌아가는 네비게이션 메서드를 삭제합니다. - 들어오는 라우팅경로에다 처리해 주기 위해 삭제한 메서드를 2가지로 분할하여 생성합니다.

--- core/navigation/src/main/java/com/gradation/lift/navigation/navigation/BadgeNavigation.kt
+++ core/navigation/src/main/java/com/gradation/lift/navigation/navigation/BadgeNavigation.kt
@@ -6,8 +6,22 @@ import com.gradation.lift.navigation.Router.BADGE_BADGE_ROUTER_NAME
 import com.gradation.lift.navigation.Router.BADGE_SETTING_ROUTER_NAME
 
 
-fun NavController.navigateBadgeGraphToPreGraph(){
-    this.popBackStack()
+fun NavController.navigateBadgeGraphToHomeGraph(){
+    this.navigate(Router.HOME_GRAPH_NAME) {
+        launchSingleTop = true
+        popUpTo(this@navigateBadgeGraphToHomeGraph.graph.id) {
+            inclusive = true
+        }
+    }
+}
+
+fun NavController.navigateBadgeGraphToMyInfoGraph(){
+    this.navigate(Router.MY_INFO_GRAPH_NAME) {
+        launchSingleTop = true
+        popUpTo(this@navigateBadgeGraphToMyInfoGraph.graph.id) {
+            inclusive = true
+        }
+    }
 }
 
 
