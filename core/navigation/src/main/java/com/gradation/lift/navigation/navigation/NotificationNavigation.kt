023cb465086bf931deb8b4da1f41546c545ddb90
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-10-03 11:00:52 +0900

    feat(navigation): update notification navigation - 요구사항에 따라 notification 네비게이션 라우팅 메서드를 수정합니다. - 푸시메시지, 공지사항 각 상세 화면에 따른 라우터 상수 값을 추가합니다.

--- core/navigation/src/main/java/com/gradation/lift/navigation/navigation/NotificationNavigation.kt
+++ core/navigation/src/main/java/com/gradation/lift/navigation/navigation/NotificationNavigation.kt
@@ -4,45 +4,28 @@ import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.gradation.lift.navigation.Router
 
-fun NavController.navigateNotificationGraphToHomeGraph() {
-    this.navigate(Router.HOME_GRAPH_NAME) {
-        launchSingleTop = true
-        popUpTo(this@navigateNotificationGraphToHomeGraph.graph.id) {
-            inclusive = true
-        }
-    }
-}
-
-fun NavController.navigateNotificationGraphToMyInfoGraph() {
-    this.navigate(Router.MY_INFO_GRAPH_NAME) {
-        launchSingleTop = true
-        popUpTo(this@navigateNotificationGraphToMyInfoGraph.graph.id) {
-            inclusive = true
-        }
-    }
+fun NavController.navigateNotificationGraphToPreGraph() {
+    this.popBackStack()
 }
 
 
-
-fun NavHostController.navigateNotificationToNoticeInNotificationGraph() {
-    this.navigate(Router.NOTIFICATION_NOTICE_ROUTER_NAME) {
-        launchSingleTop = true
-
-        popUpTo(currentDestination!!.id) {
-            saveState = true
-            inclusive = true
-        }
-    }
+fun NavHostController.navigatePreGraphToNotificationGraph() {
+    this.navigate(Router.NOTIFICATION_NOTIFICATION_ROUTER_NAME)
 }
 
-fun NavHostController.navigateNoticeToNotificationInNotificationGraph() {
-    this.navigate(Router.NOTIFICATION_NOTIFICATION_ROUTER_NAME) {
-        launchSingleTop = true
-
-        popUpTo(currentDestination!!.id) {
-            saveState = true
-            inclusive = true
-        }
-    }
+fun NavHostController.navigateNotificationToNoticeDetailInNotificationGraph() {
+    this.navigate(Router.NOTIFICATION_NOTICE_DETAIL_ROUTER_NAME)
 }
 
+fun NavHostController.navigateNotificationToPushDetailInNotificationGraph() {
+    this.navigate(Router.NOTIFICATION_PUSH_DETAIL_ROUTER_NAME)
+}
+
+
+fun NavHostController.navigateNoticeDetailToNotificationInNotificationGraph() {
+    this.popBackStack()
+}
+
+fun NavHostController.navigatePushDetailToNotificationInNotificationGraph() {
+    this.popBackStack()
+}
