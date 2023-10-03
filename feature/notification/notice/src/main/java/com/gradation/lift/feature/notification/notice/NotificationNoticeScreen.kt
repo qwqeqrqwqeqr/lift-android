4068f57d9b48b0eed9fc6ecc7641489ec295ac50
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-10-03 11:38:43 +0900

    feat(feature): update notification-notice feature - notice feature에 필수적인 뷰 구성요소(뷰모델,메인뷰)를 추가합니다.

---
+++ feature/notification/notice/src/main/java/com/gradation/lift/feature/notification/notice/NotificationNoticeScreen.kt
@@ -0,0 +1,22 @@
+package com.gradation.lift.feature.notification.notice
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun NotificationNoticeRoute(
+    modifier: Modifier = Modifier,
+    viewModel: NotificationNoticeViewModel = hiltViewModel(),
+) {
+
+
+}
+
+@Composable
+fun NotificationNoticeScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
+
---
+++ feature/notification/notice/src/main/java/com/gradation/lift/feature/notification/notice/NotificationNoticeViewModel.kt
@@ -0,0 +1,11 @@
+package com.gradation.lift.feature.notification.notice
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class NotificationNoticeViewModel @Inject constructor() : ViewModel() {
+
+
+}
