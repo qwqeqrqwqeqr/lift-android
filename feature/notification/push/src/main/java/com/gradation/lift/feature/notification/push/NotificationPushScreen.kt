a9682fba01b4a34b46c4da406add21930caba399
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-10-03 11:40:20 +0900

    feat(feature): update notification-push feature - push feature에 필수적인 뷰 구성요소(뷰모델,메인뷰)를 추가합니다.

---
+++ feature/notification/push/src/main/java/com/gradation/lift/feature/notification/push/NotificationPushScreen.kt
@@ -0,0 +1,22 @@
+package com.gradation.lift.feature.notification.push
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun NotificationPushRoute(
+    modifier: Modifier = Modifier,
+    viewModel: NotificationPushViewModel = hiltViewModel(),
+) {
+
+
+}
+
+@Composable
+fun NotificationPushScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
+
