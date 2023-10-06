be5670bace110c54181782cdc07c50ef6c2081f3
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-10-06 15:11:22 +0900

    feat(feature): update notification-notice-detail feature - 공지에 대한 상세화면을 추가합니다. - 해당 화면에서 공지사항에 대한 자세한 정보를 확인할 수 있습니다.

--- feature/notification/notice-detail/build.gradle.kts
+++ feature/notification/notice-detail/build.gradle.kts
@@ -18,4 +18,6 @@ dependencies {
     implementation(project(":core:test"))
     implementation(project(":core:navigation"))
     implementation(project(":core:ui"))
+
+    implementation(project(":feature:notification:notice"))
 }
--- feature/notification/notice-detail/src/main/java/com/gradation/lift/feature/notification/notice_detail/NotificationNoticeDetailNavigation.kt
+++ feature/notification/notice-detail/src/main/java/com/gradation/lift/feature/notification/notice_detail/NotificationNoticeDetailNavigation.kt
@@ -4,12 +4,16 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.gradation.lift.navigation.Router
+import com.gradation.lift.navigation.navigation.navigateNoticeDetailToNotificationInNotificationGraph
 
 fun notificationNoticeDetailScreen(navController: NavController, navGraphBuilder: NavGraphBuilder) {
     navGraphBuilder.composable(Router.NOTIFICATION_NOTICE_DETAIL_ROUTER_NAME) {
 
+        val navigateNoticeDetailToNotificationInNotificationGraph: () -> Unit =
+            { navController.navigateNoticeDetailToNotificationInNotificationGraph() }
 
         NotificationNoticeDetailRoute(
+            navController,navigateNoticeDetailToNotificationInNotificationGraph
         )
 
     }
