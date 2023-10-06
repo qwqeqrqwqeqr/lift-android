3e10550a0ae04d881fa227ea752d36ad6d8349cf
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-10-06 22:25:42 +0900

    feat(network): feat recommend work category service - 추천 운동목록들을 불러오는 서비스를 추가합니다.

---
+++ core/network/src/main/java/com/gradation/lift/network/dto/work/GetRecommendWorkCategoryResponseDto.kt
@@ -0,0 +1,26 @@
+package com.gradation.lift.network.dto.work
+
+import com.gradation.lift.model.model.work.WorkCategory
+import com.gradation.lift.model.model.work.WorkPart
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class GetRecommendWorkCategoryResponseDto(
+    @Json(name = "work_category")
+    val workCategory: List<WorkCategoryDto>
+) {
+    fun toDomain(): List<WorkCategory> =
+        this.workCategory.map {
+            WorkCategory(
+                id = it.id,
+                name = it.name,
+                workPart = WorkPart(
+                    id = it.workPart.id,
+                    name = it.workPart.name
+                ),
+                introduce = it.introduce,
+                description = it.description
+            )
+        }
+}
