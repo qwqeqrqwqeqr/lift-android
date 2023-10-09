5c34dd6c6e9647cfa7d6c6f21fe3506384ac1a04
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-10-09 12:47:55 +0900

    feat(design-system): update progress bar component - 프로그레스바 컴포넌트를 그리는 방식을 수정합니다.

--- core/designsystem/src/main/java/com/gradation/lift/designsystem/canvas/ProgressBar.kt
+++ core/designsystem/src/main/java/com/gradation/lift/designsystem/canvas/ProgressBar.kt
@@ -1,14 +1,16 @@
 package com.gradation.lift.designsystem.canvas
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.gradation.lift.designsystem.theme.LiftMaterialTheme
@@ -20,19 +22,39 @@ fun LiftProgressBar(
     progress: Int,
 ) {
     Canvas(modifier = modifier.padding(16.dp)) {
-        drawLine(
-            color = Color(0xFFF2F2F5),
-            start = Offset(x = 0f, y = center.y),
-            end = Offset(x = size.width, y = center.y),
-            strokeWidth = 64f,
-            cap = StrokeCap.Round
+        drawPath(
+            path = Path().apply {
+                moveTo(
+                    0f,
+                    center.y
+                )
+                lineTo(
+                    size.width, center.y
+                )
+                close()
+            },
+            style = Stroke(width = 24.dp.toPx(), cap = StrokeCap.Round, join = StrokeJoin.Round),
+            color = Color(0xFFF2F2F5)
         )
-        drawLine(
-            color = Color(0xFF0080FF),
-            start = Offset(x = 0f, y = center.y),
-            end = Offset(x = progress.toFloat() / 100 * size.width, y = center.y),
-            strokeWidth = 64f,
-            cap = StrokeCap.Round
+
+        drawPath(
+            path = Path().apply {
+                moveTo(
+                    0f,
+                    center.y
+                )
+                lineTo(
+                    progress.toFloat() / 100 * size.width, center.y
+                )
+                close()
+            },
+            style = Stroke(width = 24.dp.toPx(), cap = StrokeCap.Round, join = StrokeJoin.Round),
+            brush = Brush.horizontalGradient(
+                colors = listOf(
+                    Color(0xFF0080FF),
+                    Color(0xffffffff)
+                ), startX = 0f, endX = size.width
+            )
         )
     }
 }
