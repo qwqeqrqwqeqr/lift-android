4656e16baabf74aa3d3220c77130992ce48b96ea
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-10-12 11:08:28 +0900

    feat(feature): update create-routine-routine feature view component
    - ProfilePictureView.kt를 더이상 사용하지 않기 떄문에 제거합니다.
    - NavigationView가 아닌 CreateRoutineView로 컴포넌트 이름을 수정합니다.
    - keypad 업데이트및 주어진 요구사항 변경에 따라 RoutineListView 레이아웃들을 수정합니다.

--- feature/create-routine/routine/src/main/java/com/gradation/lift/create_routine/routine/component/CreateRoutineView.kt
+++ feature/create-routine/routine/src/main/java/com/gradation/lift/create_routine/routine/component/CreateRoutineView.kt
@@ -1,30 +1,38 @@
 package com.gradation.lift.create_routine.routine.component
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.gradation.lift.create_routine.routine.data.model.IndexWorkSet
 import com.gradation.lift.designsystem.component.LiftButton
+import com.gradation.lift.designsystem.resource.LiftIcon
 import com.gradation.lift.designsystem.theme.LiftTheme
 import com.gradation.lift.model.model.work.WorkSet
 
 @Composable
-fun NavigationView(
+fun CreateRoutineView(
     modifier: Modifier = Modifier,
-    createRoutineCondition: Boolean,
-    workSetList: List<IndexWorkSet>,
+    indexWorkSetList: List<IndexWorkSet>,
     addRoutine: (List<WorkSet>) -> Unit,
+    createRoutineCondition: Boolean,
     navigateRoutineToRoutineSetInCreateRoutineGraph: () -> Unit,
 ) {
     LiftButton(
         modifier = modifier.fillMaxWidth(),
         onClick = {
             addRoutine(
-                workSetList.map {
+                indexWorkSetList.map {
                     WorkSet(
-                        weight = it.weight.toFloat(),
-                        repetition = it.repetition.toInt()
+                        weight = it.weight.value.toFloat(),
+                        repetition = it.repetition.value.toInt()
                     )
                 }
             )
@@ -36,6 +44,14 @@ fun NavigationView(
             text = "등록하기",
             style = LiftTheme.typography.no3,
             color = LiftTheme.colorScheme.no5,
+        )
+        Spacer(modifier = modifier.padding(2.dp))
+        Icon(
+            painterResource(id = LiftIcon.ChevronRight),
+            contentDescription = "CreateRoutine",
+            modifier = modifier
+                .fillMaxHeight()
+                .width(8.dp)
         )
     }
 }
