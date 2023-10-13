5d3659ee9efe84a0b7694a64d2cbcf47c79efbe0
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-10-13 11:21:37 +0900

    feat(model): update routine service - getRoutineSetRoutineByWeekday api 주소를 변경합니다. - getRoutineSetRoutineByRoutineSetId api 주소를 변경합니다.

--- core/network/src/main/java/com/gradation/lift/network/service/RoutineService.kt
+++ core/network/src/main/java/com/gradation/lift/network/service/RoutineService.kt
@@ -67,18 +67,18 @@ interface RoutineService {
     /**
      * [getRoutineSetRoutineByWeekday]
      * 특정 요일에 대한 모든 루틴 세트 불러오기
-     * @param weekday 3글자료 표기한 요일 (ex. Mon,Tue,Wed,...)
-     * @since 2023-08-28 22:31:17
+     * @param weekday 3글자료 표기한 요일을 ,로 분류한 문자열 (ex. Mon,Tue,Wed,...)
+     * @since 2023-10-13 11:21:09
      */
-    @GET("routine/routine-set-routine-by-weekday/")
+    @GET("routine/routine-set-routine/weekday")
     suspend fun getRoutineSetRoutineByWeekday(@Query("weekday") weekday: String): Response<APIResultWrapper<GetRoutineSetRoutineByWeekdayResponseDto>>
 
     /**
      * [getRoutineSetRoutineByRoutineSetId]
      * 루틴세트 고유 아이디들과 일치하는 모든 루틴 세트 불러오기
      * @param routineSetIdList 루틴세트 고유 아이디 번호 리스트,  ','로 구분 하여 [String]으로 전달함
-     * @since 2023-08-28 22:31:12
+     * @since 2023-10-13 11:21:14
      */
-    @GET("routine/routine-set-routine-by-routine-set-id/")
+    @GET("routine/routine-set-routine/routine-set-id")
     suspend fun getRoutineSetRoutineByRoutineSetId(@Query("routine_set_id_list") routineSetIdList: String): Response<APIResultWrapper<GetRoutineSetRoutineByRoutineSetIdResponseDto>>
 }
