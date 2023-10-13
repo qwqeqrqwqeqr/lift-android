97f37f68c19cff3147728190882aa5249005ccf0
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-10-13 20:48:33 +0900

    feat(design-system): feat routineLabel - 루틴라벨에 사용될 아이콘 컴포넌트를 추가합니다.

--- core/designsystem/src/main/java/com/gradation/lift/designsystem/component/label/RoutineLabel.kt
+++ core/designsystem/src/main/java/com/gradation/lift/designsystem/component/label/RoutineLabel.kt
@@ -1,167 +1,68 @@
 package com.gradation.lift.designsystem.component.label
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.gradation.lift.designsystem.resource.LiftIcon
 import com.gradation.lift.designsystem.theme.LiftMaterialTheme
-import com.gradation.lift.designsystem.theme.LiftTheme
 
 
 @Composable
-fun MondayRoutineLabel(modifier: Modifier = Modifier) {
-    Text(
-        modifier = modifier
-            .background(
-                color = LiftTheme.colorScheme.mondayBackgroundColor,
-                shape = RoundedCornerShape(size = 30.dp)
+fun RoutineLabel(modifier: Modifier = Modifier, id: Int) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        when (id) {
+            1 -> Icon(
+                painter = painterResource(LiftIcon.RoutineLabel1),
+                contentDescription = "",
+                tint = Color.Unspecified,
             )
-            .padding(start = 8.dp, top = 10.dp, end = 8.dp, bottom = 10.dp),
-        text = "월요일",
-        color = LiftTheme.colorScheme.mondayLabelColor,
-        style = LiftTheme.typography.no7.copy(fontWeight = FontWeight.Bold)
-    )
+
+            2 -> Icon(
+                painter = painterResource(LiftIcon.RoutineLabel2),
+                contentDescription = "",
+                tint = Color.Unspecified,
+            )
+
+            3 -> Icon(
+                painter = painterResource(LiftIcon.RoutineLabel3),
+                contentDescription = "",
+                tint = Color.Unspecified,
+            )
+
+            4 -> Icon(
+                painter = painterResource(LiftIcon.RoutineLabel4),
+                contentDescription = "",
+                tint = Color.Unspecified,
+            )
+
+            else -> Icon(
+                painter = painterResource(LiftIcon.RoutineLabel5),
+                contentDescription = "",
+                tint = Color.Unspecified,
+            )
+        }
+    }
 }
 
-@Composable
-fun TuesdayRoutineLabel(modifier: Modifier = Modifier) {
-    Text(
-        modifier = modifier
-            .background(
-                color = LiftTheme.colorScheme.tuesdayBackgroundColor,
-                shape = RoundedCornerShape(size = 30.dp)
-            )
-            .padding(start = 8.dp, top = 10.dp, end = 8.dp, bottom = 10.dp),
-        text = "화요일",
-        color = LiftTheme.colorScheme.tuesdayLabelColor,
-        style = LiftTheme.typography.no7.copy(fontWeight = FontWeight.Bold)
-    )
-}
-
-@Composable
-fun WednesdayRoutineLabel(modifier: Modifier = Modifier) {
-    Text(
-        modifier = modifier
-            .background(
-                color = LiftTheme.colorScheme.wednesdayBackgroundColor,
-                shape = RoundedCornerShape(size = 30.dp)
-            )
-            .padding(start = 8.dp, top = 10.dp, end = 8.dp, bottom = 10.dp),
-        text = "수요일",
-        color = LiftTheme.colorScheme.wednesdayLabelColor,
-        style = LiftTheme.typography.no7.copy(fontWeight = FontWeight.Bold)
-    )
-}
-
-@Composable
-fun ThursdayRoutineLabel(modifier: Modifier = Modifier) {
-    Text(
-        modifier = modifier
-            .background(
-                color = LiftTheme.colorScheme.thursdayBackgroundColor,
-                shape = RoundedCornerShape(size = 30.dp)
-            )
-            .padding(start = 8.dp, top = 10.dp, end = 8.dp, bottom = 10.dp),
-        text = "목요일",
-        color = LiftTheme.colorScheme.thursdayLabelColor,
-        style = LiftTheme.typography.no7.copy(fontWeight = FontWeight.Bold)
-    )
-}
-
-@Composable
-fun FridayRoutineLabel(modifier: Modifier = Modifier) {
-    Text(
-        modifier = modifier
-            .background(
-                color = LiftTheme.colorScheme.fridayBackgroundColor,
-                shape = RoundedCornerShape(size = 30.dp)
-            )
-            .padding(start = 8.dp, top = 10.dp, end = 8.dp, bottom = 10.dp),
-        text = "금요일",
-        color = LiftTheme.colorScheme.fridayLabelColor,
-        style = LiftTheme.typography.no7.copy(fontWeight = FontWeight.Bold)
-    )
-}
-
-@Composable
-fun SaturdayRoutineLabel(modifier: Modifier = Modifier) {
-    Text(
-        modifier = modifier
-            .background(
-                color = LiftTheme.colorScheme.saturdayBackgroundColor,
-                shape = RoundedCornerShape(size = 30.dp)
-            )
-            .padding(start = 8.dp, top = 10.dp, end = 8.dp, bottom = 10.dp),
-        text = "토요일",
-        color = LiftTheme.colorScheme.saturdayLabelColor,
-        style = LiftTheme.typography.no7.copy(fontWeight = FontWeight.Bold)
-    )
-}
-
-@Composable
-fun SundayRoutineLabel(modifier: Modifier = Modifier) {
-    Text(
-        modifier = modifier
-            .background(
-                color = LiftTheme.colorScheme.sundayBackgroundColor,
-                shape = RoundedCornerShape(size = 30.dp)
-            )
-            .padding(start = 8.dp, top = 10.dp, end = 8.dp, bottom = 10.dp),
-        text = "일요일",
-        color = LiftTheme.colorScheme.sundayLabelColor,
-        style = LiftTheme.typography.no7.copy(fontWeight = FontWeight.Bold)
-    )
-}
-
-@Composable
-fun AllRoutineLabel(modifier: Modifier = Modifier) {
-    Text(
-        modifier = modifier
-            .background(
-                color = LiftTheme.colorScheme.allBackgroundColor,
-                shape = RoundedCornerShape(size = 30.dp)
-            )
-            .padding(start = 8.dp, top = 10.dp, end = 8.dp, bottom = 10.dp),
-        text = "모든요일",
-        color = LiftTheme.colorScheme.allLabelColor,
-        style = LiftTheme.typography.no7.copy(fontWeight = FontWeight.Bold)
-    )
-}
-
-@Composable
-fun RecentRoutineLabel(modifier: Modifier = Modifier) {
-    Text(
-        modifier = modifier
-            .background(
-                color = LiftTheme.colorScheme.recentBackgroundColor,
-                shape = RoundedCornerShape(size = 30.dp)
-            )
-            .padding(start = 8.dp, top = 10.dp, end = 8.dp, bottom = 10.dp),
-        text = "최근",
-        color = LiftTheme.colorScheme.recentLabelColor,
-        style = LiftTheme.typography.no7.copy(fontWeight = FontWeight.Bold)
-    )
-}
 @Composable
 @Preview(showBackground = true)
 fun RoutineLabelPreview() {
     LiftMaterialTheme {
         Column {
-            MondayRoutineLabel()
-            TuesdayRoutineLabel()
-            WednesdayRoutineLabel()
-            ThursdayRoutineLabel()
-            FridayRoutineLabel()
-            SaturdayRoutineLabel()
-            SundayRoutineLabel()
-            AllRoutineLabel()
-            RecentRoutineLabel()
+            RoutineLabel(id = 1)
+            RoutineLabel(id = 2)
+            RoutineLabel(id = 3)
+            RoutineLabel(id = 4)
+            RoutineLabel(id = 5)
         }
     }
 }
