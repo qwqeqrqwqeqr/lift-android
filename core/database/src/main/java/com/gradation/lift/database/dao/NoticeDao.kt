c76d049037b644fab19936da6d89419abacc4454
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-10-15 13:23:41 +0900

    feat(database): feat notice dao - 공지사항 DB데이터를 관리 수정할 notice dao를 추가합니다.

---
+++ core/database/src/main/java/com/gradation/lift/database/dao/NoticeDao.kt
@@ -0,0 +1,20 @@
+package com.gradation.lift.database.dao
+
+import androidx.room.*
+import com.gradation.lift.database.entity.notice.NoticeEntity
+import com.gradation.lift.database.util.Constants.Entity.NOTICE_TABLE_NAME
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoticeDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAllNotice(vararg noticeEntity: NoticeEntity)
+
+    @Query("DELETE FROM '${NOTICE_TABLE_NAME}'")
+    suspend fun deleteAllUserProfilePicture()
+
+    @Query("SELECT * FROM `${NOTICE_TABLE_NAME}`")
+    fun getAllNotice(): Flow<List<NoticeEntity>>
+
+}
