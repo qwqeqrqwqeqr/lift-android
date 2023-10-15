da3a49a95be8a746ac019457c8815542df241ba4
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-10-15 13:19:05 +0900

    feat(database): update badge dao - badge dao 내 사용자의 뱃지 획득정보를 확인하는 userBadge 관련 dao를 추가합니다.

--- core/database/src/main/java/com/gradation/lift/database/dao/BadgeDao.kt
+++ core/database/src/main/java/com/gradation/lift/database/dao/BadgeDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.gradation.lift.database.entity.badge.BadgeEntity
+import com.gradation.lift.database.entity.badge.UserBadgeEntity
 import com.gradation.lift.database.util.Constants
 import kotlinx.coroutines.flow.Flow
 
@@ -16,12 +17,31 @@ interface BadgeDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAllBadge(vararg workPartEntity: BadgeEntity)
 
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUserBadge(userBadgeEntity: UserBadgeEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAllUserBadge(vararg userBadgeEntity: UserBadgeEntity)
+
+
     @Delete
     suspend fun deleteWorkPart(badgeEntity: BadgeEntity)
+    @Delete
+    suspend fun deleteUserBadge(userBadgeEntity: UserBadgeEntity)
+
 
     @Query("DELETE FROM '${Constants.Entity.BADGE_TABLE_NAME}'")
-    suspend fun deleteAllWorkPart()
+    suspend fun deleteAllBadge()
+
+    @Query("DELETE FROM '${Constants.Entity.USER_BADGE_TABLE_NAME}'")
+    suspend fun deleteAllUserBadge()
 
     @Query("SELECT * FROM `${Constants.Entity.BADGE_TABLE_NAME}`")
     fun getAllBadge(): Flow<List<BadgeEntity>>
+
+    @Query("SELECT * FROM `${Constants.Entity.USER_BADGE_TABLE_NAME}`")
+    fun getAllUserBadge(): Flow<List<UserBadgeEntity>>
+
+
 }
