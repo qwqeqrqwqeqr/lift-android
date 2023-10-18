524c35c5496f8a613ef1a3640f7bdf75beb5e007
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-10-18 12:45:20 +0900

    feat(navigation): update navigation router - 루틴 상세 정보와 관련한 새로운 화면 추가를 위해 Router 파일을 업데이트 합니다.

--- core/navigation/src/main/java/com/gradation/lift/navigation/Router.kt
+++ core/navigation/src/main/java/com/gradation/lift/navigation/Router.kt
@@ -14,8 +14,9 @@ package com.gradation.lift.navigation
  * [MY_INFO_GRAPH_NAME]
  * [NOTIFICATION_GRAPH_NAME]
  * [REGISTER_DETAIL_GRAPH_NAME]
+ * [ROUTINE_DETAIL_GRAPH_NAME]
  * [WORK_GRAPH_NAME]
- * @since 2023-09-06 17:46:31
+ * @since 2023-10-18 12:43:52
  * */
 object Router {
 
@@ -27,15 +28,13 @@ object Router {
     const val NEW_BADGE_GRAPH_NAME = "new_badge_graph"
     const val NEW_BADGE_NEW_BADGE_ROUTER_NAME = "new_badge_new_badge"
 
-
     const val CREATE_ROUTINE_GRAPH_NAME = "create_routine_graph"
     const val CREATE_ROUTINE_ROUTINE_SET_ROUTER_NAME = "create_routine_routine_set"
     const val CREATE_ROUTINE_ROUTINE_ROUTER_NAME = "create_routine_routine"
-    const val CREATE_ROUTINE_PROFILE_ROUTER_NAME = "create_routine_profile"
+    const val CREATE_ROUTINE_PROFILE_PICTURE_ROUTER_NAME = "create_routine_profile_picture"
     const val CREATE_ROUTINE_FIND_WORK_CATEGORY_ROUTER_NAME = "create_routine_find_work_category"
 
     const val UPDATE_ROUTINE_GRAPH_NAME = "update_routine_graph"
-    const val UPDATE_ROUTINE_ROUTINE_SELECTION_ROUTER_NAME = "update_routine_routine_selection"
     const val UPDATE_ROUTINE_ROUTINE_SET_ROUTER_NAME = "update_routine_routine_set"
     const val UPDATE_ROUTINE_ROUTINE_ROUTER_NAME = "update_routine_routine"
     const val UPDATE_ROUTINE_PROFILE_PICTURE_ROUTER_NAME = "update_routine_profile_picture"
@@ -68,6 +67,10 @@ object Router {
     const val REGISTER_DETAIL_HEIGHT_WEIGHT_ROUTER_NAME = "register_detail_height_weight"
     const val REGISTER_DETAIL_NAME_ROUTER_NAME = "register_detail_name"
     const val REGISTER_DETAIL_PROFILE_PICTURE_ROUTER_NAME = "register_detail_profile_picture"
+
+    const val ROUTINE_DETAIL_GRAPH_NAME = "routine_detail_graph"
+    const val ROUTINE_DETAIL_LIST_ROUTER_NAME = "routine_detail_graph"
+    const val ROUTINE_DETAIL_DETAIL_ROUTER_NAME = "routine_detail_graph"
 
     const val WORK_GRAPH_NAME = "work_graph"
     const val WORK_ROUTINE_SELECTION_ROUTER_NAME = "work_routine_selection"
