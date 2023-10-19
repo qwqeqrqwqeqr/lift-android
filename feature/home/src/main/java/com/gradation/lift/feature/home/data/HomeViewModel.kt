dbb288fb8b2a45372e7fce8cb3e4032be6b5f063
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-10-19 16:08:00 +0900

    feat(feature): update home feature - home feature의 viewmodel을 업데이트합니다. - 새롭게 추가한 routine state를 연동합니다.

--- feature/home/src/main/java/com/gradation/lift/feature/home/data/HomeViewModel.kt
+++ feature/home/src/main/java/com/gradation/lift/feature/home/data/HomeViewModel.kt
@@ -5,9 +5,11 @@ import androidx.lifecycle.viewModelScope
 import com.gradation.lift.common.model.DataState
 import com.gradation.lift.domain.usecase.badge.GetUserBadgeByConditionUseCase
 import com.gradation.lift.domain.usecase.badge.GetUserBadgeByMainFlagUseCase
+import com.gradation.lift.domain.usecase.routine.GetMostUsedRoutineSetRoutineUseCase
 import com.gradation.lift.domain.usecase.user.GetUserDetailUseCase
 import com.gradation.lift.feature.home.data.state.BadgeConditionState
 import com.gradation.lift.feature.home.data.state.BadgeUiState
+import com.gradation.lift.feature.home.data.state.RoutineUiState
 import com.gradation.lift.feature.home.data.state.UserDetailUiState
 
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -19,7 +21,9 @@ import javax.inject.Inject
  * [HomeViewModel]
  * @property userDetailUiState 사용자 상세정보 상태
  * @property badgeUiState 대표 뱃지 상태
- * @since 2023-10-18 13:43:28
+ * @property routineUiState 루틴 상태
+ * @property badgeConditionState 뱃지 획득 조건 만족여부에 따른 상태 (성공 시, 뱃지 획득하면으로 이동한다)
+ * @since 2023-10-19 15:57:10
  */
 @OptIn(ExperimentalCoroutinesApi::class)
 
@@ -27,7 +31,8 @@ import javax.inject.Inject
 class HomeViewModel @Inject constructor(
     getUserDetailUseCase: GetUserDetailUseCase,
     getUserBadgeByMainFlagUseCase: GetUserBadgeByMainFlagUseCase,
-    getUserBadgeByConditionUseCase: GetUserBadgeByConditionUseCase
+    getUserBadgeByConditionUseCase: GetUserBadgeByConditionUseCase,
+    getMostUsedRoutineSetRoutineUseCase: GetMostUsedRoutineSetRoutineUseCase,
 ) : ViewModel() {
 
 
@@ -42,7 +47,6 @@ class HomeViewModel @Inject constructor(
         initialValue = UserDetailUiState.Loading
     )
 
-
     internal val badgeUiState: StateFlow<BadgeUiState> = getUserBadgeByMainFlagUseCase().map {
         when (it) {
             is DataState.Fail -> BadgeUiState.Fail(it.message)
@@ -53,6 +57,21 @@ class HomeViewModel @Inject constructor(
         started = SharingStarted.WhileSubscribed(5_000),
         initialValue = BadgeUiState.Loading
     )
+
+    internal val routineUiState: StateFlow<RoutineUiState> =
+        getMostUsedRoutineSetRoutineUseCase().map {
+            when (it) {
+                is DataState.Fail -> RoutineUiState.Fail(it.message)
+                is DataState.Success -> {
+                    if (it.data.isEmpty()) RoutineUiState.Empty else RoutineUiState.Success(it.data)
+                }
+            }
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = RoutineUiState.Loading
+        )
+
 
     val badgeConditionState: StateFlow<BadgeConditionState> =
         getUserBadgeByConditionUseCase().map { state ->
