e087eecddd3b20088334b6bcc6173333f06030db
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-11-20 22:56:38 +0900

    feat(routine-detail-list): feat searchview - 검색을 위한 검색 뷰를 추가합니다.

---
+++ feature/routine-detail/routine-list/src/main/java/com/gradation/lift/feature/routine_detail/routine_list/ui/SearchView.kt
@@ -0,0 +1,31 @@
+package com.gradation.lift.feature.routine_detail.routine_list.ui
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.gradation.lift.designsystem.component.LiftSearchTextField
+import com.gradation.lift.designsystem.theme.LiftTheme
+import com.gradation.lift.feature.routine_detail.routine_list.data.state.SortFilterState
+
+
+@Composable
+fun SearchView(
+    modifier: Modifier = Modifier,
+    sortFilterState: SortFilterState,
+) {
+
+        LiftSearchTextField(
+            modifier = modifier.fillMaxWidth(),
+            value = sortFilterState.searchTextFilter.value,
+            onValueChange = { sortFilterState.updateSearchText },
+            placeholder = {
+                Text(
+                    text = "찾으시는 운동을 검색해주세요",
+                    color = LiftTheme.colorScheme.no2,
+                    style = LiftTheme.typography.no6,
+                )
+            }
+        )
+
+}
