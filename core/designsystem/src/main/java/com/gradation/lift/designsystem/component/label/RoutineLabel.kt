00415cb066458c7f5fb1ee138f8d42c0e445d20d
qwqeqrqwqeqr <qwqeqrqwqeqr@github.com>
2023-12-05 09:50:54 +0900

    feat(design-system): add design-system icon - 루틴 라벨이 체크 되었을 때의 아이콘들을 추가 및 정의합니다.

--- core/designsystem/src/main/java/com/gradation/lift/designsystem/component/label/RoutineLabel.kt
+++ core/designsystem/src/main/java/com/gradation/lift/designsystem/component/label/RoutineLabel.kt
@@ -1,5 +1,6 @@
 package com.gradation.lift.designsystem.component.label
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Icon
@@ -11,6 +12,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.gradation.lift.designsystem.resource.LiftIcon
 import com.gradation.lift.designsystem.theme.LiftMaterialTheme
+import com.gradation.lift.designsystem.theme.LiftTheme
 
 
 @Composable
@@ -53,16 +55,66 @@ fun RoutineLabel(modifier: Modifier = Modifier, id: Int) {
     }
 }
 
+
+@Composable
+fun CheckedRoutineLabel(modifier: Modifier = Modifier, id: Int) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        when (id) {
+            1 -> Icon(
+                painter = painterResource(LiftIcon.CheckedRoutineLabel1),
+                contentDescription = "",
+                tint = Color.Unspecified,
+            )
+
+            2 -> Icon(
+                painter = painterResource(LiftIcon.CheckedRoutineLabel2),
+                contentDescription = "",
+                tint = Color.Unspecified,
+            )
+
+            3 -> Icon(
+                painter = painterResource(LiftIcon.CheckedRoutineLabel3),
+                contentDescription = "",
+                tint = Color.Unspecified,
+            )
+
+            4 -> Icon(
+                painter = painterResource(LiftIcon.CheckedRoutineLabel4),
+                contentDescription = "",
+                tint = Color.Unspecified,
+            )
+
+            else -> Icon(
+                painter = painterResource(LiftIcon.CheckedRoutineLabel5),
+                contentDescription = "",
+                tint = Color.Unspecified,
+            )
+        }
+    }
+}
+
 @Composable
 @Preview(showBackground = true)
 fun RoutineLabelPreview() {
     LiftMaterialTheme {
         Column {
-            RoutineLabel(id = 1)
-            RoutineLabel(id = 2)
-            RoutineLabel(id = 3)
-            RoutineLabel(id = 4)
-            RoutineLabel(id = 5)
+            Column(verticalArrangement = Arrangement.spacedBy(LiftTheme.space.space8)) {
+                RoutineLabel(id = 1)
+                RoutineLabel(id = 2)
+                RoutineLabel(id = 3)
+                RoutineLabel(id = 4)
+                RoutineLabel(id = 5)
+            }
+            Column(verticalArrangement = Arrangement.spacedBy(LiftTheme.space.space8)) {
+                CheckedRoutineLabel(id = 1)
+                CheckedRoutineLabel(id = 2)
+                CheckedRoutineLabel(id = 3)
+                CheckedRoutineLabel(id = 4)
+                CheckedRoutineLabel(id = 5)
+            }
         }
     }
 }
