9a1e0e46420d575a2ad50180b8b9d00fe0274561
qwqeqrqwqeqr <qwqeqrqwqeqr@github.com>
2023-12-05 22:13:52 +0900

    feat(navigation): update navigation state handle method - state handle의 키를 통해 값을 받아올때 모든 경로를 추적하여 값을 받아오도록 변경합니다.

--- core/navigation/src/main/java/com/gradation/lift/navigation/saved_state/SavedStateHandle.kt
+++ core/navigation/src/main/java/com/gradation/lift/navigation/saved_state/SavedStateHandle.kt
@@ -3,16 +3,18 @@ package com.gradation.lift.navigation.saved_state
 import androidx.navigation.NavController
 
 
-
-fun <T> NavController.setValueSavedStateHandle(key: String, value:T){
+fun <T> NavController.setValueSavedStateHandle(key: String, value: T) {
     this.currentBackStackEntry?.let {
         with(it.savedStateHandle) {
             set(key, value)
         }
     }
 }
+
 fun <T> NavController.getValueSavedStateHandle(key: String): T? {
-    return this.previousBackStackEntry?.savedStateHandle?.get<T>(key)
+    return this.currentBackStack.value.mapNotNull {
+        it.savedStateHandle.get<T>(key)
+    }.let { if (it.isNotEmpty()) it.first() else null }
 }
 
 
