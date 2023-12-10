88b193643b10ce024e9be62cb10882468558d0b4
qwqeqrqwqeqr <qwqeqrqwqeqr@github.com>
2023-12-10 16:29:53 +0900

    feat(create-routine): update create-routine: navigation feature - 변경된 네비게이션 라우터들을 바탕으로 해당 파일을 수정합니다.

--- feature/create-routine/navigation/src/main/kotlin/com/gradation/lift/feature/createRoutine/navigation/CreateRoutineGraphBuilder.kt
+++ feature/create-routine/navigation/src/main/kotlin/com/gradation/lift/feature/createRoutine/navigation/CreateRoutineGraphBuilder.kt
@@ -1,17 +1,19 @@
 package com.gradation.lift.feature.createRoutine.navigation
 
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.navigation
-import com.gradation.lift.createRoutine.profilePicture.createRoutineProfilePictureScreen
-import com.gradation.lift.createRoutine.routine.createRoutineRoutineScreen
-import com.gradation.lift.feature.createRoutine.routineSet.createRoutineScreen
-import com.gradation.lift.feature.createRoutine.findWorkCategory.createRoutineFindWorkCategoryScreen
+import com.gradation.lift.createRoutine.profilePicture.navigation.profilePictureScreen
+import com.gradation.lift.createRoutine.routine.navigation.routineScreen
+import com.gradation.lift.feature.createRoutine.findWorkCategory.navigation.findWorkCategoryScreen
+import com.gradation.lift.feature.createRoutine.routineSet.navigation.routineSetScreen
 import com.gradation.lift.navigation.Router.CREATE_ROUTINE_GRAPH_NAME
 import com.gradation.lift.navigation.Router.CREATE_ROUTINE_ROUTINE_SET_ROUTER_NAME
 
 
 fun createRoutineGraphBuilder(
+    modifier: Modifier = Modifier,
     navController: NavController,
     navGraphBuilder: NavGraphBuilder,
 ) {
@@ -19,10 +21,11 @@ fun createRoutineGraphBuilder(
         route = CREATE_ROUTINE_GRAPH_NAME,
         startDestination = CREATE_ROUTINE_ROUTINE_SET_ROUTER_NAME,
     ) {
-        createRoutineScreen(navController, this)
-        createRoutineRoutineScreen(navController, this)
-        createRoutineProfilePictureScreen(navController, this)
-        createRoutineFindWorkCategoryScreen(navController, this)
+        routineSetScreen(modifier,navController, this)
+        findWorkCategoryScreen(modifier,navController, this)
+        profilePictureScreen(modifier,navController, this)
+
+        routineScreen(modifier,navController, this)
     }
 }
 
