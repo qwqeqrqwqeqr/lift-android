6df721c9a47d91185c72e493a07e849dac918072
qwqeqrqwqeqr <qwqeqrqwqeqr@github.com>
2023-12-12 18:03:05 +0900

    feat(work): feat work complete feature data - 운동완료 화면에서 사용될 데이터들을 정의합니다. - 해당 데이터들을 해당 피쳐의 뷰모델과 연동합니다.

---
+++ feature/work/complete/src/main/kotlin/com/gradation/lift/feature/work/complete/data/state/HistoryInfoState.kt
@@ -0,0 +1,53 @@
+package com.gradation.lift.feature.work.complete.data.state
+
+import com.gradation.lift.common.utils.Validator
+import com.gradation.lift.common.utils.historyCommentValidator
+import com.gradation.lift.feature.work.complete.data.event.HistoryInfoEvent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+
+/**
+ * [HistoryInfoState]
+ * @property score 운동 기록 자체 평가 점수
+ * @property comment 운동 기록 메모
+ * @property commentValidator 운동 메모 유효성 검증
+ * @since 2023-12-12 17:20:44
+ */
+internal data class HistoryInfoState(
+    val score: MutableStateFlow<Int> = MutableStateFlow(5),
+    val comment: MutableStateFlow<String> = MutableStateFlow(""),
+    private val viewModelScope: CoroutineScope,
+) {
+
+
+    var commentValidator: StateFlow<Validator> =
+        comment.map { it ->
+            if(it.isBlank()){
+                Validator(true, "")
+            }
+            else if (!historyCommentValidator(it)) {
+                Validator(false, "0 - 20자 사이의 글자로 입력해주세요.")
+            } else {
+                Validator(true, "")
+            }
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = Validator()
+        )
+
+    val updateComment: (String) -> Unit = {onHistoryInfoEvent(HistoryInfoEvent.UpdateComment(it))}
+    val updateScore: (Int) -> Unit = {onHistoryInfoEvent(HistoryInfoEvent.UpdateScore(it))}
+
+    private fun onHistoryInfoEvent(historyInfoEvent: HistoryInfoEvent){
+        when(historyInfoEvent){
+            is HistoryInfoEvent.UpdateComment -> comment.value = historyInfoEvent.comment
+            is HistoryInfoEvent.UpdateScore ->  score.value = historyInfoEvent.score
+        }
+    }
+}
