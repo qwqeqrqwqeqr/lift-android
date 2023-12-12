0c9de2004e7a795f16f43783c937bf011f2d776c
qwqeqrqwqeqr <qwqeqrqwqeqr@github.com>
2023-12-12 19:24:22 +0900

    build: update minSDK - minSDK 버전을 26으로 업그레이드 합니다.

--- build-system/plugin/src/main/kotlin/com/gradation/lift/plugin/config/AndroidBuildConfig.kt
+++ build-system/plugin/src/main/kotlin/com/gradation/lift/plugin/config/AndroidBuildConfig.kt
@@ -3,7 +3,7 @@ package com.gradation.lift.plugin.config
 object AndroidBuildConfig {
     const val COMPILE_SDK_VERSION = 34
     const val TARGET_SDK_VERSION = 34
-    const val MIN_SDK_VERSION = 24
+    const val MIN_SDK_VERSION = 26
     const val VERSION_CODE = 10
     const val VERSION_NAME = "1.0.0"
 }
