a2d3a8066d509d8eb0df169695aded5880a4661e
qwqeqrqwqeqr <qwqeqrqwqeqr@github.com>
2023-12-12 18:50:03 +0900

    build: update version - 1.0.0 버전을 릴리즈합니다. - 통합된 브랜치를 테스트합니다.

--- build-system/plugin/src/main/kotlin/com/gradation/lift/plugin/config/AndroidBuildConfig.kt
+++ build-system/plugin/src/main/kotlin/com/gradation/lift/plugin/config/AndroidBuildConfig.kt
@@ -3,7 +3,7 @@ package com.gradation.lift.plugin.config
 object AndroidBuildConfig {
     const val COMPILE_SDK_VERSION = 34
     const val TARGET_SDK_VERSION = 34
-    const val MIN_SDK_VERSION = 30
-    const val VERSION_CODE = 9
-    const val VERSION_NAME = "0.0.14"
+    const val MIN_SDK_VERSION = 24
+    const val VERSION_CODE = 10
+    const val VERSION_NAME = "1.0.0"
 }
