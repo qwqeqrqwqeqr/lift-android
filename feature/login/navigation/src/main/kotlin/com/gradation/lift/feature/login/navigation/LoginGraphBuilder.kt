da1bdafaed8bdc8e1fb1ffb4a3f2d9966022ee8b
qwqeqrqwqeqr <qwqeqrqwqeqr@github.com>
2023-12-24 19:37:48 +0900

    refactor(login): refactor login feature module - 사용하지 않는 verification 피쳐 모듈을 제거합니다. - 로그인 네비게이션 그래프 빌더를 재정의합니다.

--- feature/login/navigation/src/main/kotlin/com/gradation/lift/feature/login/navigation/LoginGraphBuilder.kt
+++ feature/login/navigation/src/main/kotlin/com/gradation/lift/feature/login/navigation/LoginGraphBuilder.kt
@@ -1,38 +1,27 @@
 package com.gradation.lift.feature.login.navigation
 
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.navigation
 import com.gradation.lift.feature.login.complete.loginCompleteScreen
 import com.gradation.lift.feature.login.find_email_password.loginFindEmailPasswordScreen
-import com.gradation.lift.feature.login.sign_in.loginSignInScreen
+import com.gradation.lift.feature.login.signIn.navigation.signInScreen
 import com.gradation.lift.feature.login.sign_up.loginSignUpScreen
 import com.gradation.lift.navigation.Router.LOGIN_GRAPH_NAME
 import com.gradation.lift.navigation.Router.LOGIN_SIGN_IN_ROUTER_NAME
-import com.gradation.lift.oauth.common.OAuthConnectState
-import kotlinx.coroutines.flow.MutableStateFlow
 
 
 fun loginGraphBuilder(
+    modifier: Modifier= Modifier,
     navController: NavController,
     navGraphBuilder: NavGraphBuilder,
-    naverOAuthConnectState: MutableStateFlow<OAuthConnectState>,
-    kakaoOAuthConnectState: MutableStateFlow<OAuthConnectState>,
-    connectOAuthFromNaver: () -> Unit,
-    connectOAuthFromKakao: () -> Unit,
 ) {
     navGraphBuilder.navigation(
         route = LOGIN_GRAPH_NAME,
         startDestination = LOGIN_SIGN_IN_ROUTER_NAME,
     ) {
-        loginSignInScreen(
-            navController = navController,
-            navGraphBuilder = this,
-            naverOAuthConnectState =naverOAuthConnectState,
-            kakaoOAuthConnectState =kakaoOAuthConnectState,
-            connectOAuthFromNaver =  connectOAuthFromNaver,
-            connectOAuthFromKakao =  connectOAuthFromKakao
-        )
+        signInScreen(modifier,navController, this)
         loginSignUpScreen(navController, this)
         loginCompleteScreen(navController, this)
         loginFindEmailPasswordScreen(navController, this)
