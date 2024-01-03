116663fa3a31aa26c0e94fc4a9d0ddc2bee65f87
qwqeqrqwqeqr <qwqeqrqwqeqr@github.com>
2024-01-03 20:31:51 +0900

    feat(register-detail): update gender feature - gender 피쳐를 전체적으로 수정합니다. - ui구조를 수정합니다. - 데이터 레이어 및 관련 상태들을 수정변경합니다.

---
+++ feature/register-detail/gender/src/main/kotlin/com/gradation/lift/feature/register_detail/gender/data/GenderViewModel.kt
@@ -0,0 +1,14 @@
+package com.gradation.lift.feature.register_detail.gender.data
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+/**
+ * [GenderViewModel]
+ */
+@HiltViewModel
+class GenderViewModel @Inject constructor() : ViewModel() {
+
+
+}
