22257ef1f16e44858e66ddd3c652c9a322f2d3c7
qwqeqrqwqeqr <qwqeqrqwqeqr@github.com>
2024-01-07 18:33:47 +0900

    feat(home) feat home feature common module - 홈 화면 피쳐의 모듈내에서 공통적으로 사용할 클래스 및 메서드를 관리해줄 common 모듈을 추가합니다.

--- feature/home/common/src/main/kotlin/com/gradation/lift/feature/home/common/data/BadgeConditionState.kt
+++ feature/home/common/src/main/kotlin/com/gradation/lift/feature/home/common/data/BadgeConditionState.kt
@@ -1,10 +1,10 @@
-package com.gradation.lift.feature.home.data.state
+package com.gradation.lift.feature.home.common.data
 
 import com.gradation.lift.model.model.badge.Badge
 
 sealed interface BadgeConditionState {
     data class Success(val badge: Badge) : BadgeConditionState
 
-    object None : BadgeConditionState
+    data object None : BadgeConditionState
 
 }
