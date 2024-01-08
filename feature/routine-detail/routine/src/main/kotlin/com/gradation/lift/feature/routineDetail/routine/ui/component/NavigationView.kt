f66233f11c6765d5d40d99332c541b8a41859b86
qwqeqrqwqeqr <qwqeqrqwqeqr@github.com>
2024-01-08 21:25:06 +0900

    feat(routine-detail): update routine feature - 루틴 상세화면에 대한 UI를 전체적으로 리뉴얼합니다.

--- feature/routine-detail/routine/src/main/kotlin/com/gradation/lift/feature/routineDetail/routine/ui/component/NavigationView.kt
+++ feature/routine-detail/routine/src/main/kotlin/com/gradation/lift/feature/routineDetail/routine/ui/component/NavigationView.kt
@@ -4,14 +4,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.gradation.lift.designsystem.component.LiftButton
-import com.gradation.lift.designsystem.component.LiftOutlineButton
+import androidx.compose.ui.graphics.RectangleShape
+import com.gradation.lift.designsystem.component.button.LiftPrimaryButton
+import com.gradation.lift.designsystem.component.button.LiftSolidButton
+import com.gradation.lift.designsystem.component.container.LiftDefaultContainer
 import com.gradation.lift.designsystem.theme.LiftTheme
 import com.gradation.lift.model.model.routine.RoutineSetRoutine
 
@@ -22,37 +21,26 @@ fun NavigationView(
     navigateRoutineDetailGraphToUpdateRoutineGraph: (Int) -> Unit,
     navigateRoutineDetailGraphToWorkWorkRouter: (Int) -> Unit,
 ) {
-    Row(
-        modifier = modifier
-            .background(LiftTheme.colorScheme.no5)
-            .fillMaxWidth()
-            .padding(LiftTheme.space.paddingSpace),
-        horizontalArrangement = Arrangement.spacedBy(LiftTheme.space.space10),
-        verticalAlignment = Alignment.Bottom
+    LiftDefaultContainer(
+        modifier = modifier.background(LiftTheme.colorScheme.no5).fillMaxWidth(),
+        shape = RectangleShape,
+        verticalPadding = LiftTheme.space.space10,
+        horizontalPadding = LiftTheme.space.space20
     ) {
-        LiftOutlineButton(
-            modifier = modifier.weight(1f),
-            shape = RoundedCornerShape(size = LiftTheme.space.space12),
-            onClick = {
-                navigateRoutineDetailGraphToUpdateRoutineGraph(routineSetRoutine.id)
-            },
+        Row(
+            modifier = modifier,
+            horizontalArrangement = Arrangement.spacedBy(LiftTheme.space.space10),
+            verticalAlignment = Alignment.Bottom
         ) {
-            Text(
+            LiftPrimaryButton(
+                modifier = modifier.weight(1f),
                 text = "루틴 수정",
-                style = LiftTheme.typography.no3,
-                color = LiftTheme.colorScheme.no4,
+                onClick = { navigateRoutineDetailGraphToUpdateRoutineGraph(routineSetRoutine.id) }
             )
-        }
-        LiftButton(
-            modifier = modifier.weight(1f),
-            onClick = {
-                navigateRoutineDetailGraphToWorkWorkRouter(routineSetRoutine.id)
-            },
-        ) {
-            Text(
+            LiftSolidButton(
+                modifier = modifier.weight(1f),
                 text = "운동 시작",
-                style = LiftTheme.typography.no3,
-                color = LiftTheme.colorScheme.no5,
+                onClick = { navigateRoutineDetailGraphToWorkWorkRouter(routineSetRoutine.id) }
             )
         }
     }
--- feature/routine-detail/routine/src/main/kotlin/com/gradation/lift/feature/routineDetail/routine/ui/component/RoutineContentView.kt
+++ feature/routine-detail/routine/src/main/kotlin/com/gradation/lift/feature/routineDetail/routine/ui/component/RoutineContentView.kt
@@ -1,13 +1,11 @@
 package com.gradation.lift.feature.routineDetail.routine.ui.component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -25,6 +23,7 @@ import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.gradation.lift.designsystem.component.text.LiftText
 import com.gradation.lift.designsystem.component.text.LiftTextStyle
+import com.gradation.lift.designsystem.component.textField.LiftDefaultInputTextField
 import com.gradation.lift.designsystem.resource.LiftIcon
 import com.gradation.lift.designsystem.theme.LiftTheme
 import com.gradation.lift.model.model.date.getWeekdayEntries
@@ -36,7 +35,7 @@ import com.gradation.lift.model.model.routine.RoutineSetRoutine
 fun RoutineContentView(
     modifier: Modifier = Modifier,
     routineSetRoutine: RoutineSetRoutine,
-){
+) {
     Column(
         modifier = modifier
             .fillMaxWidth()
@@ -47,7 +46,7 @@ fun RoutineContentView(
         LiftText(
             textStyle = LiftTextStyle.No3,
             text = "루틴 프로필",
-            color = LiftTheme.colorScheme.no3,
+            color = LiftTheme.colorScheme.no10,
             textAlign = TextAlign.Start
         )
         GlideImage(
@@ -55,73 +54,39 @@ fun RoutineContentView(
                 .size(LiftTheme.space.space96)
                 .align(Alignment.CenterHorizontally),
             model = routineSetRoutine.picture,
-            contentDescription = "RoutineProfilePicture"
+            contentDescription = "routineProfilePicture"
         )
         LiftText(
             textStyle = LiftTextStyle.No3,
             text = "루틴 이름",
-            color = LiftTheme.colorScheme.no3,
+            color = LiftTheme.colorScheme.no10,
             textAlign = TextAlign.Start
         )
-        Box(
-            modifier = modifier
-                .border(
-                    width = LiftTheme.space.space2,
-                    color = LiftTheme.colorScheme.no8,
-                    shape = RoundedCornerShape(size = LiftTheme.space.space12)
-                )
-                .height(LiftTheme.space.space48)
-                .background(
-                    color = LiftTheme.colorScheme.no1,
-                    shape = RoundedCornerShape(size = LiftTheme.space.space12)
-                )
-                .fillMaxWidth()
-                .padding(horizontal = LiftTheme.space.space14),
-            contentAlignment = Alignment.CenterStart
-        ) {
-            LiftText(
-                textStyle = LiftTextStyle.No6,
-                text = routineSetRoutine.name,
-                color = LiftTheme.colorScheme.no9,
-                textAlign = TextAlign.Start
-            )
-        }
+        LiftDefaultInputTextField(
+            value = routineSetRoutine.name,
+            onValueChange = {},
+            enabled = false
+        )
+
+
+
         if (routineSetRoutine.description.isNotEmpty()) {
             LiftText(
                 textStyle = LiftTextStyle.No3,
                 text = "루틴 설명",
-                color = LiftTheme.colorScheme.no3,
+                color = LiftTheme.colorScheme.no10,
                 textAlign = TextAlign.Start
             )
-
-            Box(
-                modifier = modifier
-                    .border(
-                        width = LiftTheme.space.space2,
-                        color = LiftTheme.colorScheme.no8,
-                        shape = RoundedCornerShape(size = LiftTheme.space.space12)
-                    )
-                    .height(LiftTheme.space.space48)
-                    .background(
-                        color = LiftTheme.colorScheme.no1,
-                        shape = RoundedCornerShape(size = LiftTheme.space.space12)
-                    )
-                    .fillMaxWidth()
-                    .padding(horizontal = LiftTheme.space.space14),
-                contentAlignment = Alignment.CenterStart
-            ) {
-                LiftText(
-                    textStyle = LiftTextStyle.No6,
-                    text = routineSetRoutine.description,
-                    color = LiftTheme.colorScheme.no9,
-                    textAlign = TextAlign.Start
-                )
-            }
+            LiftDefaultInputTextField(
+                value = routineSetRoutine.description,
+                onValueChange = {},
+                enabled = false
+            )
         }
         LiftText(
             textStyle = LiftTextStyle.No3,
             text = "루틴 요일",
-            color = LiftTheme.colorScheme.no3,
+            color = LiftTheme.colorScheme.no10,
             textAlign = TextAlign.Start
         )
         LazyRow(
@@ -181,7 +146,7 @@ fun RoutineContentView(
         LiftText(
             textStyle = LiftTextStyle.No3,
             text = "루틴 라벨",
-            color = LiftTheme.colorScheme.no3,
+            color = LiftTheme.colorScheme.no10,
             textAlign = TextAlign.Start
         )
 
@@ -219,13 +184,13 @@ fun RoutineContentView(
             ).forEach {
                 if (routineSetRoutine.label.contains(it.first))
                     Icon(
-                        modifier = modifier.size(LiftTheme.space.space28),
+                        modifier = modifier.size(LiftTheme.space.space24),
                         painter = painterResource(id = it.third),
                         contentDescription = "",
                         tint = Color.Unspecified
                     )
                 else Icon(
-                    modifier = modifier.size(LiftTheme.space.space28),
+                    modifier = modifier.size(LiftTheme.space.space24),
                     painter = painterResource(id = it.second),
                     contentDescription = "",
                     tint = Color.Unspecified
@@ -235,7 +200,7 @@ fun RoutineContentView(
         LiftText(
             textStyle = LiftTextStyle.No3,
             text = "운동 목록",
-            color = LiftTheme.colorScheme.no3,
+            color = LiftTheme.colorScheme.no10,
             textAlign = TextAlign.Start
         )
     }
