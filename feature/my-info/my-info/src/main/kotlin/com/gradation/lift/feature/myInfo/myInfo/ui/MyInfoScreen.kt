a009ac073c968eec2facbbb58e2a3f96d525140e
qwqeqrqwqeqr <qwqeqrqwqeqr@github.com>
2024-01-12 14:20:02 +0900

    feat(my-info): update my info feature - my info ui를 업데이트합니다. - 기존 프로필 뷰를 삭제하고 새롭게 헤더부분을 구성합니다. - 커스텀 Default Button의 파라미터를 수정하여 더욱 다양한 버튼을 제작할 수 있도록 합니다.

--- feature/my-info/my-info/src/main/kotlin/com/gradation/lift/feature/myInfo/myInfo/ui/MyInfoScreen.kt
+++ feature/my-info/my-info/src/main/kotlin/com/gradation/lift/feature/myInfo/myInfo/ui/MyInfoScreen.kt
@@ -1,67 +1,101 @@
 package com.gradation.lift.feature.myInfo.myInfo.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.gradation.lift.designsystem.component.LiftErrorSnackBar
+import androidx.compose.ui.text.style.TextAlign
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import com.gradation.lift.designsystem.component.button.LiftDefaultButton
+import com.gradation.lift.designsystem.component.snackbar.LiftSnackBar
+import com.gradation.lift.designsystem.component.text.LiftText
+import com.gradation.lift.designsystem.component.text.LiftTextStyle
 import com.gradation.lift.designsystem.theme.LiftTheme
-import com.gradation.lift.feature.myInfo.myInfo.ui.component.MyInfoListView
-import com.gradation.lift.feature.myInfo.myInfo.ui.component.ProfileView
-import com.gradation.lift.feature.myInfo.myInfo.data.state.UserDetailUiState
+import com.gradation.lift.feature.myInfo.myInfo.data.state.MyInfoScreenState
+import com.gradation.lift.feature.myInfo.myInfo.data.state.MyInfoUiState
 
 
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun MyInfoScreen(
     modifier: Modifier = Modifier,
-    versionName: String,
-    badgeCount: Int,
-    workCount: Int,
-    userDetailUiState: UserDetailUiState,
-    signOut: () -> Unit,
-    navigateProfileToUpdateProfilePictureInMyInfoGraph: () -> Unit,
-    navigateMyInfoToUpdateInMyInfoGraph: () -> Unit,
-    navigateMyInfoGraphToNotificationGraph: () -> Unit,
-    navigateMyInfoGraphToBadgeGraph: () -> Unit,
-    snackbarHostState: SnackbarHostState
+    navigateMyInfoGraphToNoticeGraph: () -> Unit,
+    navigateMyInfoToProfileInMyInfoGraph: () -> Unit,
+    myInfoUiState: MyInfoUiState,
+    myInfoScreenState: MyInfoScreenState,
 ) {
     Scaffold(
         snackbarHost = {
-            LiftErrorSnackBar(
+            LiftSnackBar(
                 modifier = modifier,
-                snackbarHostState = snackbarHostState
+                snackbarHostState = myInfoScreenState.snackbarHostState
             )
         }
     ) { padding ->
-        Surface(
-            modifier = modifier.padding(padding),
-            color = LiftTheme.colorScheme.no17,
+        Column(
+            modifier = modifier
+                .background(LiftTheme.colorScheme.no17)
+                .fillMaxSize()
+                .padding(padding),
+            verticalArrangement = Arrangement.spacedBy(LiftTheme.space.space8)
         ) {
-            Column(modifier = modifier.fillMaxSize()) {
-                ProfileView(
-                    modifier,
-                    userDetailUiState,
-                    badgeCount,
-                    workCount,
-                    signOut,
-                    navigateProfileToUpdateProfilePictureInMyInfoGraph,
-                    navigateMyInfoGraphToNotificationGraph,
-                    navigateMyInfoGraphToBadgeGraph
-                )
-                Spacer(modifier = modifier.padding(9.dp))
-                MyInfoListView(
-                    modifier,
-                    versionName,
-                    navigateMyInfoToUpdateInMyInfoGraph,
-                    navigateMyInfoGraphToNotificationGraph,
-                )
-
+            when (myInfoUiState) {
+                is MyInfoUiState.Fail -> {}
+                MyInfoUiState.Loading -> {}
+                is MyInfoUiState.Success -> {
+                    Row(
+                        modifier = modifier
+                            .fillMaxWidth()
+                            .background(LiftTheme.colorScheme.no5)
+                            .padding(
+                                top = LiftTheme.space.space40,
+                                start = LiftTheme.space.space20,
+                                end = LiftTheme.space.space20,
+                                bottom = LiftTheme.space.space16,
+                            ),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Row(
+                            modifier = modifier,
+                            horizontalArrangement = Arrangement.spacedBy(
+                                LiftTheme.space.space12,
+                                Alignment.CenterHorizontally
+                            ),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            GlideImage(
+                                modifier = modifier.size(LiftTheme.space.space36),
+                                model = myInfoUiState.myInfoState.profilePicture,
+                                contentDescription = "profilePicture"
+                            )
+                            LiftText(
+                                modifier = modifier,
+                                textStyle = LiftTextStyle.No3,
+                                text = myInfoUiState.myInfoState.name,
+                                color = LiftTheme.colorScheme.no3,
+                                textAlign = TextAlign.Start
+                            )
+                        }
+                        LiftDefaultButton(
+                            modifier = modifier,
+                            width = LiftTheme.space.space80,
+                            height = LiftTheme.space.space36,
+                            shape = RoundedCornerShape(LiftTheme.space.space8),
+                            text = "프로필 보기",
+                            onClick = navigateMyInfoToProfileInMyInfoGraph
+                        )
+                    }
+                }
             }
         }
     }
