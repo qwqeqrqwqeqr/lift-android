e6fa2eb8dfd3a94143809a67484d4814ea2942e8
qwqeqrqwqeqr <qwqeqrqwqeqr@github.com>
2024-01-21 15:12:29 +0900

    test(network): update datasource test - 데이터 소스 클래스에 요구되는 파라미터를 추가합니다.

--- core/network/src/test/java/com/gradation/lift/network/test/datasource/UserDataSourceTest.kt
+++ core/network/src/test/java/com/gradation/lift/network/test/datasource/UserDataSourceTest.kt
@@ -55,7 +55,7 @@ class UserDataSourceTest {
             NetworkResultHandler(dispatcherProvider = dispatcher)
         userDataSource = DefaultUserDataSource(
             userService = userService,
-            networkResultHandler = networkResultHandler
+            networkResultHandler = networkResultHandler,dispatcher
         )
     }
 
