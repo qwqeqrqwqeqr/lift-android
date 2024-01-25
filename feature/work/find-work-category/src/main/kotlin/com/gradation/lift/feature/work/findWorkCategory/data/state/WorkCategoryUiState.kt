b9eb58a3bb41134edd8feba22b48c101ef3a202c
qwqeqrqwqeqr <qwqeqrqwqeqr@github.com>
2024-01-25 14:44:02 +0900

    feat(work): update find work category feature module - 운동 카테고리를 찾는 feature를 업데이트합니다. - 해당 화면에 대한 데이터와 UI를 업데이트합니다.

---
+++ feature/work/find-work-category/src/main/kotlin/com/gradation/lift/feature/work/findWorkCategory/data/state/WorkCategoryUiState.kt
@@ -0,0 +1,77 @@
+package com.gradation.lift.feature.work.findWorkCategory.data.state
+
+import com.gradation.lift.common.model.DataState
+import com.gradation.lift.domain.usecase.work.GetPopularWorkCategoryUseCase
+import com.gradation.lift.domain.usecase.work.GetRecommendWorkCategoryUseCase
+import com.gradation.lift.domain.usecase.work.GetWorkCategoryUseCase
+import com.gradation.lift.feature.work.findWorkCategory.data.model.TagWorkCategory
+import com.gradation.lift.model.model.work.WorkPart
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
+
+fun workCategoryUiState(
+    workPartFilter: MutableStateFlow<Set<WorkPart>>,
+    searchFilterText: MutableStateFlow<String>,
+    getWorkCategoryUseCase: GetWorkCategoryUseCase,
+    getPopularWorkCategoryUseCase: GetPopularWorkCategoryUseCase,
+    getRecommendWorkCategoryUseCase: GetRecommendWorkCategoryUseCase
+): Flow<WorkCategoryUiState> {
+    return combine(
+        workPartFilter,
+        searchFilterText,
+        getWorkCategoryUseCase(),
+        getPopularWorkCategoryUseCase(),
+        getRecommendWorkCategoryUseCase()
+
+    ) { workPart,
+        text,
+        workCategoryList,
+        popularWorkCategoryList,
+        recommendWorkCategoryList ->
+        when (popularWorkCategoryList) {
+            is DataState.Fail -> WorkCategoryUiState.Fail(popularWorkCategoryList.message)
+            is DataState.Success -> {
+                when (recommendWorkCategoryList) {
+                    is DataState.Fail -> WorkCategoryUiState.Fail(recommendWorkCategoryList.message)
+                    is DataState.Success -> {
+                        when (workCategoryList) {
+                            is DataState.Fail -> WorkCategoryUiState.Fail(workCategoryList.message)
+                            is DataState.Success -> {
+                                WorkCategoryUiState.Success(
+                                    workCategoryList.data
+                                        .filter { workCategory ->
+                                            text.isEmpty() || workCategory.name.contains(
+                                                text
+                                            )
+                                        }
+                                        .filter { workCategory ->
+                                            workPart.isEmpty() || workPart.contains(
+                                                workCategory.workPart
+                                            )
+                                        }.sortedBy { it.name }.map { workCategory ->
+                                            TagWorkCategory(
+                                                workCategory,
+                                                popularWorkCategoryList.data.contains(
+                                                    workCategory
+                                                ),
+                                                recommendWorkCategoryList.data.contains(
+                                                    workCategory
+                                                ),
+                                            )
+                                        }
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+sealed interface WorkCategoryUiState {
+    data class Success(val workCategoryList: List<TagWorkCategory>) : WorkCategoryUiState
+    data class Fail(val message: String) : WorkCategoryUiState
+    data object Loading : WorkCategoryUiState
+}
