de863a5d2d42bf1c9dcf21cbee61d923dbb80b50
qwqeqrqwqeqr <qwqeqrqwqeqr@github.com>
2024-02-19 20:04:37 +0900

    feat(my-info): update cancel membership confirm feature - 최종적으로 회원 탈퇴를 진행하는 화면을 구현합니다. - 해당 화면에서 요구되는 데이터들을 정의하여 구현합니다.

--- feature/my-info/cancel-membership-confirm/src/main/kotlin/com/gradation/lift/feature/myInfo/cancelMembershipConfirm/navigation/CancelMembershipConfirmRoute.kt
+++ feature/my-info/cancel-membership-confirm/src/main/kotlin/com/gradation/lift/feature/myInfo/cancelMembershipConfirm/navigation/CancelMembershipConfirmRoute.kt
@@ -1,20 +1,69 @@
 package com.gradation.lift.feature.myInfo.cancelMembershipConfirm.navigation
 
+import android.annotation.SuppressLint
 import androidx.activity.compose.BackHandler
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.gradation.lift.feature.myInfo.cancelMembershipConfirm.data.CancelMembershipConfirmViewModel
+import com.gradation.lift.feature.myInfo.cancelMembershipConfirm.data.DeleteUserState
 import com.gradation.lift.feature.myInfo.cancelMembershipConfirm.ui.CancelMembershipConfirmScreen
+import com.gradation.lift.ui.extensions.showImmediatelySnackbar
+import com.gradation.lift.ui.provider.LocalAppScope
+import com.gradation.lift.ui.provider.LocalInfoSnackbarHostState
+import kotlinx.coroutines.launch
 
 
+@SuppressLint("CoroutineCreationDuringComposition")
 @Composable
 fun CancelMembershipConfirmRoute(
     modifier: Modifier = Modifier,
     navigateCancelMembershipConfirmToCancelMembershipInMyInfoGraph: () -> Unit,
     navigateMyInfoGraphToLoginGraph: () -> Unit,
+    viewModel: CancelMembershipConfirmViewModel = hiltViewModel(),
 ) {
-    BackHandler(onBack = {})
+
+    val name: String by viewModel.name.collectAsStateWithLifecycle()
+    val deleteUserState: DeleteUserState by viewModel.deleteUserState.collectAsStateWithLifecycle()
+
+    val updateDeleteUserState: (DeleteUserState) -> Unit = viewModel.updateDeleteUserState
+    val deleteUser: () -> Unit = viewModel.deleteUser
+
+    val snackbarHostState: SnackbarHostState = remember { SnackbarHostState() }
+    val infoAppSnackbarHostState = LocalInfoSnackbarHostState.current
+    val appScope = LocalAppScope.current
+
+
+    LaunchedEffect(deleteUserState) {
+        when (val result = deleteUserState) {
+            is DeleteUserState.Fail -> {
+                snackbarHostState.showImmediatelySnackbar(result.message)
+                updateDeleteUserState(DeleteUserState.None)
+            }
+
+            DeleteUserState.None -> {}
+            DeleteUserState.Success -> {
+                appScope.launch {
+                    infoAppSnackbarHostState.showImmediatelySnackbar("리프트 탈퇴가 완료됐습니다. 다음에 다시 만나요. \uD83D\uDE01")
+                }
+                navigateMyInfoGraphToLoginGraph()
+            }
+        }
+    }
+
+
+    BackHandler(onBack = navigateCancelMembershipConfirmToCancelMembershipInMyInfoGraph)
 
     CancelMembershipConfirmScreen(
         modifier,
+        name,
+        deleteUser,
+        snackbarHostState,
+        navigateCancelMembershipConfirmToCancelMembershipInMyInfoGraph
     )
 }
--- feature/my-info/cancel-membership-confirm/src/main/kotlin/com/gradation/lift/feature/myInfo/cancelMembershipConfirm/ui/CancelMembershipConfirmScreen.kt
+++ feature/my-info/cancel-membership-confirm/src/main/kotlin/com/gradation/lift/feature/myInfo/cancelMembershipConfirm/ui/CancelMembershipConfirmScreen.kt
@@ -1,36 +1,93 @@
 package com.gradation.lift.feature.myInfo.cancelMembershipConfirm.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import com.gradation.lift.designsystem.component.button.LiftErrorButton
+import com.gradation.lift.designsystem.component.snackbar.LiftSnackBar
+import com.gradation.lift.designsystem.component.text.LiftMultiStyleText
+import com.gradation.lift.designsystem.component.text.LiftTextStyle
+import com.gradation.lift.designsystem.component.text.TextWithStyle
 import com.gradation.lift.designsystem.component.topBar.LiftTopBar
 import com.gradation.lift.designsystem.theme.LiftTheme
 
 @Composable
 fun CancelMembershipConfirmScreen(
     modifier: Modifier = Modifier,
+    name: String,
+    deleteUser: () -> Unit,
+    snackbarHostState: SnackbarHostState,
+    navigateCancelMembershipConfirmToCancelMembershipInMyInfoGraph: () -> Unit,
 ) {
     Scaffold(
         topBar = {
             LiftTopBar(
-                title = "약관 및 정책",
-                onClick = {}
+                title = "탈퇴하기",
+                onClick = navigateCancelMembershipConfirmToCancelMembershipInMyInfoGraph
             )
         },
+        snackbarHost = {
+            LiftSnackBar(
+                modifier = modifier,
+                snackbarHostState = snackbarHostState
+            )
+        }
     ) { padding ->
         Column(
             modifier = modifier
                 .fillMaxSize()
                 .background(LiftTheme.colorScheme.no5)
                 .padding(padding)
-                .padding(top = LiftTheme.space.space32),
-
+                .padding(
+                    top = LiftTheme.space.space36,
+                    start = LiftTheme.space.space20,
+                    end = LiftTheme.space.space20
+                ),
+            verticalArrangement = Arrangement.spacedBy(LiftTheme.space.space24)
+        ) {
+            Column(
+                modifier = modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(LiftTheme.space.space16)
             ) {
-
+                LiftMultiStyleText(
+                    modifier,
+                    LiftTheme.colorScheme.no11,
+                    LiftTextStyle.No1,
+                    listOf(
+                        TextWithStyle(text = "정말로 리프트를 "),
+                        TextWithStyle(
+                            text = "탈퇴",
+                            color = LiftTheme.colorScheme.no12,
+                        ),
+                        TextWithStyle(text = "하실건가요?")
+                    ),
+                    TextAlign.Center
+                )
+                LiftMultiStyleText(
+                    modifier,
+                    LiftTheme.colorScheme.no3,
+                    LiftTextStyle.No6,
+                    listOf(
+                        TextWithStyle(text = "${name}님, 계정을 삭제하면 "),
+                        TextWithStyle(
+                            text = "모든 활동 정보가 삭제되며 삭제 후 7일간 다시 가입할 수 없어요.",
+                            color = LiftTheme.colorScheme.no12,
+                        ),
+                    ),
+                    TextAlign.Start
+                )
+            }
+            LiftErrorButton(text = "계정 삭제", onClick = deleteUser)
         }
     }
 }
