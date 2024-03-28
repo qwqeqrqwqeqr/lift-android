c36ebb4edfbf3047d55136df3aed6119d9a607f7
qwqeqrqwqeqr <qwqeqrqwqeqr@github.com>
2024-03-28 19:41:51 +0900

    fix: fix create work set navigation - 루틴 생성화면에서 운동 세트 추가시 정상적인 동작이 일어나지 않던 문제를 수정합니다.

--- build-system/plugin/src/main/kotlin/com/gradation/lift/plugin/config/AndroidBuildConfig.kt
+++ build-system/plugin/src/main/kotlin/com/gradation/lift/plugin/config/AndroidBuildConfig.kt
@@ -4,6 +4,6 @@ object AndroidBuildConfig {
     const val COMPILE_SDK_VERSION = 34
     const val TARGET_SDK_VERSION = 34
     const val MIN_SDK_VERSION = 26
-    const val VERSION_CODE = 16
-    const val VERSION_NAME = "1.0.6"
+    const val VERSION_CODE = 17
+    const val VERSION_NAME = "1.0.7"
 }
